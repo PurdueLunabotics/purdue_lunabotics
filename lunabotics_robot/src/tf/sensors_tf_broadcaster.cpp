9c63649bd820ee4fe44c8189e437c0325e6c20cd
Raghava <raghava.upp13@gmail.com>
2021-01-17 19:41:10 -0600

    update launch file and tf tree

--- lunabotics_robot/src/tf/sensors_tf_broadcaster.cpp
+++ lunabotics_robot/src/tf/sensors_tf_broadcaster.cpp
@@ -12,15 +12,15 @@ int main(int argc, char** argv){
   while(n.ok()){
     broadcaster.sendTransform(
       tf::StampedTransform(
-        tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.495, 0.172, 0.127)),
+        tf::Transform(tf::Quaternion(0, 0, 1, 0), tf::Vector3(-0.495,0.127,0.172)),
         ros::Time::now(),"base_link", "camera_left"));
     broadcaster.sendTransform(
       tf::StampedTransform(
-        tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.495, -0.172, 0.127)),
+        tf::Transform(tf::Quaternion(0, 0, 1, 0), tf::Vector3(-0.495, -0.127, 0.172)),
         ros::Time::now(),"base_link", "camera_right"));
 		broadcaster.sendTransform(
       tf::StampedTransform(
-        tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.495, 0.150, 0.173)),
+        tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.495, 0.173, 0.150)),
         ros::Time::now(),"base_link", "lidar"));
 		r.sleep();
   }
