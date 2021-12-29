63de3c1297f388a5f673baa77e40acce1f61f184
Ashvin Iyer <ashvin.iyer@gmail.com>
2021-12-29 15:20:50 -0500

    Adding changes

---
+++ lunabot_nav/src/simulation/simulation.py
@@ -0,0 +1,20 @@
+from robot import Robot
+from renderer import Renderer
+
+class Simulation:
+    def __init__(self, robot_length, robot_width, field_length, field_width, pixels_per_meter, start_x=0, start_y=0, start_angle=0): # All in meters
+        self.robot = Robot(robot_width, start_x, start_y, start_angle)
+        self.renderer = Renderer(robot_length, robot_width, field_length, field_width, pixels_per_meter)
+        self.simulate = True
+    
+    def update(self, left_vel, right_vel, delta_t):
+        if(self.simulate):
+            self.robot.update_pos(left_vel, right_vel, delta_t)
+            print(self.robot.x, self.robot.y)
+            self.renderer.render(self.robot.x, self.robot.y, self.robot.heading)
+    
+    def start(self):
+        self.simulate = True
+    
+    def stop(self):
+        self.simulate = False
