efdc01a384f67b9a5e766762b1aaa4e87ec0d2e5
Isaac-F24 <isaacfuksman@gmail.com>
2023-03-25 16:04:00 -0400

    Refactoring dstar

--- lunabot_nav/scripts/dstar_node.py
+++ lunabot_nav/scripts/dstar_node.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 
-import rospy
-from geometry_msgs.msg import Twist, PoseStamped
-from std_msgs.msg import Float32MultiArray
-from nav_msgs.msg import Odometry, OccupancyGrid, Path
-from map_msgs.msg import OccupancyGridUpdate
-from tf.transformations import euler_from_quaternion, quaternion_from_euler
-import dstar_ros
 import numpy as np
-import matplotlib.pyplot as plt
+import rospy
+from geometry_msgs.msg import PoseStamped
+from map_msgs.msg import OccupancyGridUpdate
+from nav_msgs.msg import OccupancyGrid, Odometry, Path
+from tf.transformations import quaternion_from_euler
+
+from lunabot_nav.dstar import Dstar
 
 pose = []
 
@@ -26,6 +25,7 @@ x_offset = 0
 
 y_offset = 0
 
+
 def grid_subscriber(data):
     global update_map, map, res, x_offset, y_offset
     width = data.info.width
@@ -36,18 +36,20 @@ def grid_subscriber(data):
     y_offset = data.info.origin.position.y
     update_map = True
 
+
 def grid_update_subscriber(data):
     global update_map, map, res, x_offset, y_offset
 
     temp_map = map.copy()
     index = 0
-    for i in range (data.y, data.y+data.height):
-        for j in range (data.x, data.x+data.width):
+    for i in range(data.y, data.y + data.height):
+        for j in range(data.x, data.x + data.width):
             temp_map[i][j] = data.data[index]
-            index+=1
+            index += 1
 
     map = temp_map.copy()
     update_map = True
+
 
 def position_subscriber(data):
     global pose
@@ -56,28 +58,36 @@ def position_subscriber(data):
     coords = [position.x, position.y]
     pose = coords
 
+
 def goal_subscriber(data):
     global goal, need_update_goal
     print("New Goal")
     goal = [data.pose.position.x, data.pose.position.y]
     need_update_goal = True
 
+
 def main():
     global map, update_map, pose, goal, res, need_update_goal
 
-    rospy.init_node('dstar_ros_script')
+    rospy.init_node("dstar_ros_script")
 
-    radius = 8 #robot rad (grid units)
+    radius = 8  # robot rad (grid units)
 
-    frequency = 10 #hz
+    frequency = 10  # hz
 
     dstar = None
 
-    path_publisher = rospy.Publisher("/nav/global_path", Path, queue_size = 10)
+    path_publisher = rospy.Publisher("/nav/global_path", Path, queue_size=10)
 
-    rospy.Subscriber("/maps/costmap_node/global_costmap/costmap", OccupancyGrid, grid_subscriber)
+    rospy.Subscriber(
+        "/maps/costmap_node/global_costmap/costmap", OccupancyGrid, grid_subscriber
+    )
 
-    rospy.Subscriber("/maps/costmap_node/global_costmap/costmap_updates", OccupancyGridUpdate, grid_update_subscriber)
+    rospy.Subscriber(
+        "/maps/costmap_node/global_costmap/costmap_updates",
+        OccupancyGridUpdate,
+        grid_update_subscriber,
+    )
 
     rospy.Subscriber("/odom", Odometry, position_subscriber)
 
@@ -90,31 +100,31 @@ def main():
     path = []
     while not rospy.is_shutdown():
         # print("Loop")
-        if (dstar == None and len(map) > 0 and len(pose) > 0 and len(goal) > 0):
-            dstar = dstar_ros.Dstar(goal, pose, map, radius, res, x_offset, y_offset)
+        if (dstar is None) and len(map) > 0 and len(pose) > 0 and len(goal) > 0:
+            dstar = Dstar(goal, pose, map, radius, res, x_offset, y_offset)
             print("Initialize")
 
-        if (dstar != None):
+        if dstar is not None:
             # print("Iterate")
 
             dstar.update_position(pose)
 
-            if (need_update_goal):
+            if need_update_goal:
                 print("created new Dstar")
-                dstar = dstar_ros.Dstar(goal, pose, map, radius, res, x_offset, y_offset)
+                dstar = Dstar(goal, pose, map, radius, res, x_offset, y_offset)
                 completedInitialRun = False
                 # dstar.update_goal(goal)
                 need_update_goal = False
-            
-            if (update_map):
+
+            if update_map:
                 dstar.update_map(map, x_offset, y_offset)
                 update_map = False
 
-            if (not completedInitialRun):
+            if not completedInitialRun:
                 dstar.find_path(True)
                 completedInitialRun = True
 
-            if (dstar.needs_new_path):
+            if dstar.needs_new_path:
                 print("Start create path")
                 path = np.array(dstar.createPathList())
                 print("End create path")
@@ -124,22 +134,20 @@ def main():
                 # else:
                 #     print("published path")
 
-                temp = map.copy()
                 ros_struct = Path()
                 ros_struct.poses = []
                 ros_struct.header.stamp = rospy.Time.now()
                 ros_struct.header.frame_id = "odom"
 
-                count = 0
                 for index, point in enumerate(path):
-                    if (index % 5 ==0 or np.all(path[index] == path[-1])):
+                    if index % 5 == 0 or np.all(path[index] == path[-1]):
 
                         path_pose = PoseStamped()
 
                         path_pose.pose.position.x = point[0]
                         path_pose.pose.position.y = point[1]
 
-                        rotation = quaternion_from_euler(0,0,0)
+                        rotation = quaternion_from_euler(0, 0, 0)
                         path_pose.pose.orientation.x = rotation[0]
                         path_pose.pose.orientation.y = rotation[1]
                         path_pose.pose.orientation.z = rotation[2]
@@ -147,12 +155,12 @@ def main():
 
                         ros_struct.poses.append(path_pose)
 
-                
                 # plt.imshow(temp, cmap='hot', interpolation='nearest')
                 # plt.show()
                 path_publisher.publish(ros_struct)
 
         rate.sleep()
+
 
 if __name__ == "__main__":
     main()
