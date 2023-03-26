424cadc88b25ff41979c072974c49c85e6b65779
Raghava Uppuluri <raghava.upp13@gmail.com>
2023-03-26 13:46:22 -0400

    new controls compile

--- lunabot_control/firmware/teensy_main/teensy_main.ino
+++ lunabot_control/firmware/teensy_main/teensy_main.ino
@@ -6,22 +6,37 @@
 ros::NodeHandle nh;
 
 void effort_cb(const lunabot_msgs::RobotEffort &effort) {
-  actuation::cb(effort.lead_screw, effort.lin_act);
-  drivetrain::cb(effort.left_drive, effort.right_drive);
-  deposition::cb(effort.deposit);
-  excavation::cb(effort.excavate);
+    actuation::cb(effort.lead_screw, effort.lin_act);
+    drivetrain::cb(effort.left_drive, effort.right_drive);
+    deposition::cb(effort.deposit);
+    excavation::cb(effort.excavate);
 }
 
 ros::Subscriber<lunabot_msgs::RobotEffort> effort_sub("/effort", effort_cb);
 
+/*
+ros::Publisher effort_pub("/current/act_lead_screw");
+ros::Publisher effort_pub("/current/act_left");
+ros::Publisher effort_pub("/current/excavation");
+ros::Publisher effort_pub("/current/deposition");
+ros::Publisher effort_pub("/current/drive_left");
+ros::Publisher effort_pub("/current/drive_right");
+
+// ros::Publisher effort_pub("/encoder/actuation");
+// ros::Publisher effort_pub("/encoder/drive_left");
+// ros::Publisher effort_pub("/encoder/drive_right");
+// ros::Publisher effort_pub("/encoder/lin_act_left");
+*/
+
+void publish() {}
+
 void setup() {
-  STMotorInterface::init_serial(ST_SERIAL, ST_BAUD_RATE);
-  nh.initNode();
-  nh.subscribe(effort_sub);
+    STMotorInterface::init_serial(ST_SERIAL, ST_BAUD_RATE);
+    nh.initNode();
+    nh.subscribe(effort_sub);
 }
 
 void loop() {
-  nh.spinOnce();
-  actuation::run();
-  delay(10);
+    nh.spinOnce();
+    delay(10);
 }
