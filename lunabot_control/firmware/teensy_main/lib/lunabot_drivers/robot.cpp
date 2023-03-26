bd76b9af114761af273a386fb6a7f46e00652e63
Raghava Uppuluri <raghava.upp13@gmail.com>
2023-03-26 13:46:22 -0400

    new controls compile

--- lunabot_control/firmware/teensy_main/lib/lunabot_drivers/robot.cpp
+++ lunabot_control/firmware/teensy_main/lib/lunabot_drivers/robot.cpp
@@ -1,29 +1,26 @@
 #include <robot.hpp>
 
-Sabertooth MC1(128, ST_SERIAL);
-Sabertooth MC2(129, ST_SERIAL);
+Sabertooth MC1(128, ST_SERIAL); // top
+Sabertooth MC2(129, ST_SERIAL); // middle
+Sabertooth MC3(130, ST_SERIAL); // bottom
+Sabertooth MC4(131, ST_SERIAL); // high-current
 
 namespace actuation {
-static StepperDir lead_screw_dir;
-StepperInterface lead_screw{19, 22, 20, 23, 200, 90, 30};
-STMotorInterface lin_acts{&MC2, STMotor::M1};
+STMotorInterface act_right{&MC2, STMotor::M1};
+STMotorInterface act_left{&MC2, STMotor::M2};
+STMotorInterface lead_screw_motor{&MC1, STMotor::M1};
 
-void run() { lead_screw.step(lead_screw_dir); }
-
-void cb(int8_t lead_screw_pwm, int8_t lin_act_pwm) {
-    lead_screw_dir = (lead_screw_pwm > 0) ? EXTEND : RETRACT;
-    lin_acts.write(lin_act_pwm);
-    if (lead_screw_pwm != 0) {
-        lead_screw.on();
-    } else {
-        lead_screw.off();
-    }
+void cb(int8_t lead_screw, int8_t lin_act) {
+    act_left.write(lin_act);
+    act_right.write(lin_act);
+    lead_screw_motor.write(lead_screw);
 }
+
 } // namespace actuation
 
 namespace drivetrain {
-STMotorInterface left_drive{&MC1, STMotor::M1};
-STMotorInterface right_drive{&MC1, STMotor::M2};
+STMotorInterface left_drive{&MC3, STMotor::M1};
+STMotorInterface right_drive{&MC3, STMotor::M2};
 void cb(int8_t left, int8_t right) {
     // Tank drive steering
     left_drive.write(left);
@@ -32,19 +29,11 @@ void cb(int8_t left, int8_t right) {
 } // namespace drivetrain
 
 namespace excavation {
-STMotorInterface exc_motor{&MC2, STMotor::M1};
+STMotorInterface exc_motor{&MC4, STMotor::M1};
 void cb(int8_t speed) { exc_motor.write(speed); }
 } // namespace excavation
 
 namespace deposition {
-
-MotorInterface dep_motor{13, 14};
-
-void cb(int8_t speed) {
-    MotorDir dep_dir =
-        (speed > 0)
-            ? CCW
-            : CW; // CCW rotates deposition up, CW retracts deposition down
-    dep_motor.write(speed, dep_dir);
-}
+STMotorInterface dep_motor{&MC1, STMotor::M2};
+void cb(int8_t speed) { dep_motor.write(speed); }
 } // namespace deposition
