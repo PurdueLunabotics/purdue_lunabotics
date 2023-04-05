812f5d42ad9066fe6d55fbe1805840003a572652
Raghava Uppuluri <raghava.upp13@gmail.com>
2023-04-04 22:21:00 -0400

    compiles and not tested

--- lunabot_control/firmware/teensy_main/lib/lunabot_drivers/robot.hpp
+++ lunabot_control/firmware/teensy_main/lib/lunabot_drivers/robot.hpp
@@ -8,21 +8,31 @@
 #define ST_SERIAL Serial1
 #define ST_BAUD_RATE 9600
 
+extern ADS1115_lite adc0;
+extern ADS1115_lite adc1;
+
 namespace actuation {
 void cb(int8_t lead_screw, int8_t lin_act);
-void run();
+void loop_once();
+void update(int16_t *, int16_t *);
 } // namespace actuation
 
 namespace drivetrain {
 void cb(int8_t left, int8_t right);
-}
+void loop_once();
+void update(int16_t *, int16_t *);
+} // namespace drivetrain
 
 namespace deposition {
 void cb(int8_t dep);
-}
+void loop_once();
+void update(int16_t *);
+} // namespace deposition
 
 namespace excavation {
 void cb(int8_t exc);
-}
+void loop_once();
+void update(int16_t *);
+} // namespace excavation
 
 #endif
--- lunabot_control/firmware/teensy_main/teensy_main.ino
+++ lunabot_control/firmware/teensy_main/teensy_main.ino
@@ -2,6 +2,7 @@
 #include "ros.h"
 #include <lunabot_msgs/RobotEffort.h>
 #include <robot.hpp>
+#include <std_msgs/Int16.h>
 
 ros::NodeHandle nh;
 
@@ -14,29 +15,56 @@ void effort_cb(const lunabot_msgs::RobotEffort &effort) {
 
 ros::Subscriber<lunabot_msgs::RobotEffort> effort_sub("/effort", effort_cb);
 
-/*
-ros::Publisher effort_pub("/current/act_lead_screw");
-ros::Publisher effort_pub("/current/act_left");
-ros::Publisher effort_pub("/current/excavation");
-ros::Publisher effort_pub("/current/deposition");
-ros::Publisher effort_pub("/current/drive_left");
-ros::Publisher effort_pub("/current/drive_right");
+std_msgs::Int16 lead_screw_curr_msg;
+std_msgs::Int16 act_right_curr_msg;
+std_msgs::Int16 exc_curr_msg;
+std_msgs::Int16 dep_curr_msg;
+std_msgs::Int16 drive_left_curr_msg;
+std_msgs::Int16 drive_right_curr_msg;
+
+ros::Publisher lead_screw_curr_pub("/current/act_lead_screw",
+                                   &lead_screw_curr_msg);
+ros::Publisher act_right_curr_pub("/current/act_right", &act_right_curr_msg);
+ros::Publisher exc_curr_pub("/current/excavation", &exc_curr_msg);
+ros::Publisher dep_curr_pub("/current/deposition", &dep_curr_msg);
+ros::Publisher drive_left_curr_pub("/current/drive_left", &drive_left_curr_msg);
+ros::Publisher drive_right_curr_pub("/current/drive_right",
+                                    &drive_right_curr_msg);
+
+void publish_state() {
+    actuation::update(&(act_right_curr_msg.data), &(lead_screw_curr_msg.data));
+    drivetrain::update(&(drive_left_curr_msg.data),
+                       &(drive_right_curr_msg.data));
+    excavation::update(&(exc_curr_msg.data));
+    deposition::update(&(dep_curr_msg.data));
+
+    act_right_curr_pub.publish(&act_right_curr_msg);
+    drive_left_curr_pub.publish(&drive_left_curr_msg);
+    drive_right_curr_pub.publish(&drive_right_curr_msg);
+    exc_curr_pub.publish(&exc_curr_msg);
+    dep_curr_pub.publish(&dep_curr_msg);
+}
 
 // ros::Publisher effort_pub("/encoder/actuation");
 // ros::Publisher effort_pub("/encoder/drive_left");
 // ros::Publisher effort_pub("/encoder/drive_right");
 // ros::Publisher effort_pub("/encoder/lin_act_left");
-*/
-
-void publish() {}
+// ros::Publisher effort_pub("/encoder/lin_act_right");
 
 void setup() {
     STMotorInterface::init_serial(ST_SERIAL, ST_BAUD_RATE);
+    CurrentSensor::init_ads1115(&adc0);
+    CurrentSensor::init_ads1115(&adc1);
     nh.initNode();
     nh.subscribe(effort_sub);
 }
 
 void loop() {
     nh.spinOnce();
-    delay(10);
+    actuation::loop_once();
+    drivetrain::loop_once();
+    deposition::loop_once();
+    excavation::loop_once();
+    publish_state();
+    delay(50);
 }
