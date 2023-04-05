a9dc7286995a52fc8decc8c1722a950b30cf4dce
Raghava <raghava.upp13@gmail.com>
2023-04-04 22:59:42 -0400

    ensure 50ms ctrl loop with sensors

--- lunabot_control/firmware/teensy_main/teensy_main.ino
+++ lunabot_control/firmware/teensy_main/teensy_main.ino
@@ -7,10 +7,10 @@
 ros::NodeHandle nh;
 
 void effort_cb(const lunabot_msgs::RobotEffort &effort) {
-    actuation::cb(effort.lead_screw, effort.lin_act);
-    drivetrain::cb(effort.left_drive, effort.right_drive);
-    deposition::cb(effort.deposit);
-    excavation::cb(effort.excavate);
+  actuation::cb(effort.lead_screw, effort.lin_act);
+  drivetrain::cb(effort.left_drive, effort.right_drive);
+  deposition::cb(effort.deposit);
+  excavation::cb(effort.excavate);
 }
 
 ros::Subscriber<lunabot_msgs::RobotEffort> effort_sub("/effort", effort_cb);
@@ -32,17 +32,16 @@ ros::Publisher drive_right_curr_pub("/current/drive_right",
                                     &drive_right_curr_msg);
 
 void publish_state() {
-    actuation::update(&(act_right_curr_msg.data), &(lead_screw_curr_msg.data));
-    drivetrain::update(&(drive_left_curr_msg.data),
-                       &(drive_right_curr_msg.data));
-    excavation::update(&(exc_curr_msg.data));
-    deposition::update(&(dep_curr_msg.data));
+  actuation::update(&(act_right_curr_msg.data), &(lead_screw_curr_msg.data));
+  drivetrain::update(&(drive_left_curr_msg.data), &(drive_right_curr_msg.data));
+  excavation::update(&(exc_curr_msg.data));
+  deposition::update(&(dep_curr_msg.data));
 
-    act_right_curr_pub.publish(&act_right_curr_msg);
-    drive_left_curr_pub.publish(&drive_left_curr_msg);
-    drive_right_curr_pub.publish(&drive_right_curr_msg);
-    exc_curr_pub.publish(&exc_curr_msg);
-    dep_curr_pub.publish(&dep_curr_msg);
+  act_right_curr_pub.publish(&act_right_curr_msg);
+  drive_left_curr_pub.publish(&drive_left_curr_msg);
+  drive_right_curr_pub.publish(&drive_right_curr_msg);
+  exc_curr_pub.publish(&exc_curr_msg);
+  dep_curr_pub.publish(&dep_curr_msg);
 }
 
 // ros::Publisher effort_pub("/encoder/actuation");
@@ -52,11 +51,11 @@ void publish_state() {
 // ros::Publisher effort_pub("/encoder/lin_act_right");
 
 void setup() {
-    STMotorInterface::init_serial(ST_SERIAL, ST_BAUD_RATE);
-    CurrentSensor::init_ads1115(&adc0);
-    CurrentSensor::init_ads1115(&adc1);
-    nh.initNode();
-    nh.subscribe(effort_sub);
+  STMotorInterface::init_serial(ST_SERIAL, ST_BAUD_RATE);
+  CurrentSensor::init_ads1115(&adc0);
+  CurrentSensor::init_ads1115(&adc1);
+  nh.initNode();
+  nh.subscribe(effort_sub);
 }
 
 void loop() {
