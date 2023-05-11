6f00e4d23327e96d49a1e4ea2f3ee9bc8f4a18b7
Raghava Uppuluri <raghava.upp13@gmail.com>
2023-05-11 11:23:00 -0400

    Squashed commit of the following:
    commit 1a0501051bdd9d28ac30981eb7ed39270e0c7de0
    Author: Raghava Uppuluri <raghava.upp13@gmail.com>
    Date:   Thu May 11 10:56:47 2023 -0400
    
        add back manual_control.launch
    
    commit acf68faaabb7c40cab37178ffd30537ede06d9c7
    Author: Raghava Uppuluri <raghava.upp13@gmail.com>
    Date:   Thu May 11 10:47:31 2023 -0400
    
        add image_proc rosdep
    
    commit abe83a3461d25d30f75b43901483be2f3eb4a1fd
    Author: Raghava <raghava.upp13@gmail.com>
    Date:   Thu May 11 00:37:18 2023 -0400
    
        working diff drive control on robot, not tuned
    
    commit 9b7a5ed864f029ff245a35aa3d8de4b1e537c11c
    Author: Raghava Uppuluri <raghava.upp13@gmail.com>
    Date:   Fri May 5 11:59:54 2023 -0400
    
        update topic names
    
    commit 0eb21bea2949cb60d1db62cf21921d5c471b21dc
    Author: Ashvin Iyer <aniyer@purdue.edu>
    Date:   Wed May 3 17:15:48 2023 -0400
    
        Adding proper velocity limits
    
    commit f0debe36008768607ab8187896d18e3538444ebd
    Author: Ashvin Iyer <aniyer@purdue.edu>
    Date:   Wed May 3 17:11:50 2023 -0400
    
        Changing encoder to float32 and adding main functin
    
    commit 4cc2c79f2a9def81f78d3cb9a5acef083dc6d625
    Author: Ashvin Iyer <aniyer@purdue.edu>
    Date:   Wed May 3 16:53:57 2023 -0400
    
        Adding differential drive controller, has option to use encoder velocity PID
    
    Co-authored-by: Ashvin Iyer <aniyer@purdue.edu>

---
+++ lunabot_control/scripts/differential_drive_controller.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+import numpy as np
+import rospy
+from geometry_msgs.msg import Twist
+
+from lunabot_msgs.msg import RobotEffort, RobotState
+
+
+class DifferentialDriveController:
+    width = 0.5588  # Robot width, TODO find
+    max_speed_percentage = 1  # Maximum speed we're allowing drive motors to spin
+
+    def __init__(self):
+
+        # ROS Publishers and subsribers to get / send data
+
+        self._vel_sub = rospy.Subscriber("/cmd_vel", Twist, self._vel_cb)
+        self._effort_pub = rospy.Publisher("/effort", RobotEffort, queue_size=1)
+        self._state_sub = rospy.Subscriber("state", RobotState, self._robot_state_cb)
+
+        # Variables for PIDF Velocity Control
+
+        self.prev_left_vel_reading = 0
+        self.prev_right_vel_reading = 0
+        self.prev_left_reading = 0
+        self.left_reading = 0
+        self.prev_right_reading = 0
+        self.right_reading = 0
+
+    def _vel_cb(self, vel_msg):
+        lin = vel_msg.linear.x
+        ang = vel_msg.angular.z
+        effort_msg = RobotEffort()
+
+        left = lin - ang * self.width / 2
+        right = lin + ang * self.width / 2
+
+        # left, right = self._vel_pidf(left, right) #Using encoder velocity pid
+
+        effort_msg.left_drive = self.constrain(left)
+        effort_msg.right_drive = self.constrain(right)
+        self._effort_pub.publish(effort_msg)
+
+    def _robot_state_cb(self, msg):
+        self.right_prev_reading = self.right_reading
+        self.right_reading = msg.drive_right_ang
+
+        self.left_prev_reading = self.left_reading
+        self.left_reading = msg.drive_left_ang
+
+    def _vel_pidf(self, left_vel, right_vel):
+        max_speed = 1  # TODO find max speed of motors in meters / second
+        p = 0  # P gain for PD controller
+        d = 0  # D gain for PD controller
+
+        # Feed forward
+        left_percent_estimate = np.clip(left_vel / max_speed, -1, 1)
+        right_percent_estimate = np.clip(right_vel / max_speed, -1, 1)
+
+        meters_per_tick = 1  # TODO find
+        encoder_dt = 0.01  # Amount of time between readings
+        loop_dt = 0.1  # Amount of time between calls of this function
+
+        # Measured Velocity in m / s
+        left_vel_reading = (
+            (self.left_reading - self.prev_left_reading) / encoder_dt * meters_per_tick
+        )
+        right_vel_reading = (
+            (self.right_reading - self.prev_right_reading)
+            / encoder_dt
+            * meters_per_tick
+        )
+
+        # Calculating error
+        left_error = left_vel - left_vel_reading
+        right_error = right_vel - right_vel_reading
+
+        # Calculating previous error
+        left_prev_error = left_vel - self.prev_left_vel_reading
+        right_prev_error = right_vel - self.prev_right_vel_reading
+
+        # Calculating motor velocities
+        left = (
+            left_percent_estimate
+            + left_error * p
+            + (left_error - left_prev_error) / loop_dt * d
+        )
+        right = (
+            right_percent_estimate
+            + right_error * p
+            + (right_error - right_prev_error) / loop_dt * d
+        )
+
+        # Updating previous readings
+        self.prev_left_vel_reading = left_vel_reading
+        self.prev_right_vel_reading = right_vel_reading
+
+        return left, right
+
+    def constrain(self, val):
+        val = np.clip(-1, val, 1)  # Clipping speed to not go over 100%
+        return np.int8(val * 127 * self.max_speed_percentage)
+
+
+if __name__ == "__main__":
+    rospy.init_node("differential_drive_controller")
+
+    controller = DifferentialDriveController()
+    rospy.spin()
