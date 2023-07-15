2501d3481d0ee3989dd3dbf39056819a7fd48b2f
Raghava Uppuluri <raghava.upp13@gmail.com>
2023-07-15 14:50:25 -0400

    update dependencies for CI

--- install-non-ros-deps.sh
+++ install-non-ros-deps.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 apt-get update  # To get the latest package lists
-apt-get install libusb-dev -y
+apt-get install libusb-dev libtool dh-autoreconf libudev-dev -y
+
+git clone https://github.com/Microsoft/vcpkg.git
+cd vcpkg
+./bootstrap-vcpkg.sh
+./vcpkg integrate install
+./vcpkg install realsense2
