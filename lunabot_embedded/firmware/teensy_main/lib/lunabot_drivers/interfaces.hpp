084984960da06b2621e05efe3d5205c6054186e3
WoozyDragon <WoozyDragon@gmail.com>
2023-10-24 22:04:04 -0400

    Added UWB code to teensy     - Added new class in interfaces             - Followed pattern of encoder class             - Sourced from uwb_sensor_test.ino     - Used class in robot.cpp in new namespace             - Followed pattern of other namespaces             - No read command; unecessary     - Added new interrupt timer in teensy_main             - To run code from interfaces

--- lunabot_embedded/firmware/teensy_main/lib/lunabot_drivers/interfaces.hpp
+++ lunabot_embedded/firmware/teensy_main/lib/lunabot_drivers/interfaces.hpp
@@ -3,97 +3,109 @@
 #include <SPI.h>
 #include <Sabertooth.h>
 #include <Stepper.h>
+#include <Wire.h>
 
 #ifndef __INTERFACES_H__
-#define __INTERFACES_H__ 
+#define __INTERFACES_H__
 enum StepperDir { RETRACT = -1, EXTEND = 1 };
 enum MotorDir { CW = HIGH, CCW = LOW };
 enum STMotor { M1 = 1, M2 = 2 };
 
 class StepperInterface {
-  public:
-    StepperInterface(uint8_t PWM1_P, uint8_t PWM2_P, uint8_t DIR1_P,
-                     uint8_t DIR2_P, int steps, int speed, int step_size);
-    void on();
-    void off();
-    void step(StepperDir dir);
+public:
+  StepperInterface(uint8_t PWM1_P, uint8_t PWM2_P, uint8_t DIR1_P,
+                   uint8_t DIR2_P, int steps, int speed, int step_size);
+  void on();
+  void off();
+  void step(StepperDir dir);
 
-  private:
-    Stepper s_;
-    uint8_t PWM1_P_;
-    uint8_t PWM2_P_;
-    uint8_t DIR1_P_;
-    uint8_t DIR2_P_;
-    int en_;
-    int steps_;     // steps per revolution
-    int speed_;     // steps per minute
-    int step_size_; // step cnt per step
+private:
+  Stepper s_;
+  uint8_t PWM1_P_;
+  uint8_t PWM2_P_;
+  uint8_t DIR1_P_;
+  uint8_t DIR2_P_;
+  int en_;
+  int steps_;     // steps per revolution
+  int speed_;     // steps per minute
+  int step_size_; // step cnt per step
 };
 
 class MotorInterface {
-  public:
-    MotorInterface(uint8_t PWM_P, uint8_t DIR_P);
-    void write(uint8_t pwm, MotorDir dir);
+public:
+  MotorInterface(uint8_t PWM_P, uint8_t DIR_P);
+  void write(uint8_t pwm, MotorDir dir);
 
-  private:
-    uint8_t PWM_P_;
-    uint8_t DIR_P_;
+private:
+  uint8_t PWM_P_;
+  uint8_t DIR_P_;
 };
 
 class STMotorInterface {
-  public:
-    STMotorInterface(Sabertooth *st, STMotor motor);
-    void write(int8_t pwm);
-    static void init_serial(HardwareSerial s, int baud_rate);
+public:
+  STMotorInterface(Sabertooth *st, STMotor motor);
+  void write(int8_t pwm);
+  static void init_serial(HardwareSerial s, int baud_rate);
 
-  private:
-    static int initialized_serial_;
-    Sabertooth *st_;
-    STMotor motor_;
+private:
+  static int initialized_serial_;
+  Sabertooth *st_;
+  STMotor motor_;
 };
 
 // Sensors
 
-
 class CurrentSensorBus {
-  public:
-    CurrentSensorBus(){};
-    static void init_ads1115();
-    static int16_t read(uint8_t bus, uint8_t mux);
-    static void transfer();
+public:
+  CurrentSensorBus(){};
+  static void init_ads1115();
+  static int16_t read(uint8_t bus, uint8_t mux);
+  static void transfer();
 
-  private:
-    static constexpr int BUSES = 2;
-    static constexpr int CH_SIZE = 4;
-    static constexpr int BUS_SIZE = 4;
-    static const int ADS_CHANNELS[CH_SIZE];
-    static int initialized_;
-    static ADS1115_lite adc0_;
-    static ADS1115_lite adc1_;
-    static uint8_t adc0_ch_;
-    static uint8_t adc1_ch_;
-    static int16_t curr_buffer_[BUSES][BUS_SIZE];
+private:
+  static constexpr int BUSES = 2;
+  static constexpr int CH_SIZE = 4;
+  static constexpr int BUS_SIZE = 4;
+  static const int ADS_CHANNELS[CH_SIZE];
+  static int initialized_;
+  static ADS1115_lite adc0_;
+  static ADS1115_lite adc1_;
+  static uint8_t adc0_ch_;
+  static uint8_t adc1_ch_;
+  static int16_t curr_buffer_[BUSES][BUS_SIZE];
+};
+
+class UWBBus {
+public:
+  UWBBus(){};
+  static void init();
+  static float read_uwb(uint8_t id);
+  static void transfer();
+
+private:
+  static constexpr int BUS_SIZE = 3;
+  volatile static float uwb_buffer_[BUS_SIZE];
 };
 
 class EncoderBus {
-  public:
-    EncoderBus(){};
-    static void init();
-    static float read_enc(uint8_t id);
-    static void transfer();
+public:
+  EncoderBus(){};
+  static void init();
+  static float read_enc(uint8_t id);
+  static void transfer();
 
-  private:
-    static constexpr int BUFFER_SIZE = 2;
-    static constexpr int BUS_SIZE = 5;
-    static constexpr int sel0_p_ = 8;
-    static constexpr int sel1_p_ = 9;
-    static constexpr int sel2_p_ = 10;
-    static constexpr int clk_p_ = 13;
-    volatile static uint8_t curr_id_;
-    volatile static uint8_t spi_buffer_[BUFFER_SIZE];
-    volatile static uint32_t enc_buffer_[BUS_SIZE];
+private:
+  static constexpr int BUFFER_SIZE = 2;
+  static constexpr int BUS_SIZE = 5;
+  static constexpr int sel0_p_ = 8;
+  static constexpr int sel1_p_ = 9;
+  static constexpr int sel2_p_ = 10;
+  static constexpr int clk_p_ = 13;
+  volatile static uint8_t curr_id_;
+  volatile static uint8_t spi_buffer_[BUFFER_SIZE];
+  volatile static uint32_t enc_buffer_[BUS_SIZE];
 
-    static void select_enc_(uint8_t id);
+  static void select_enc_(uint8_t id);
 };
 
 #endif
--- lunabot_embedded/firmware/teensy_main/teensy_main.ino
+++ lunabot_embedded/firmware/teensy_main/teensy_main.ino
@@ -5,10 +5,10 @@
 #include <robot.hpp>
 #include <stdio.h>
 
-#define TX_PERIOD 10             // ms
-#define ENC_TRANSFER_PERIOD 1000 // microsec
-#define CURR_UPDATE_PERIOD 8 // ms
-
+#define TX_PERIOD 10               // ms
+#define ENC_TRANSFER_PERIOD 1000   // microsec
+#define UWB_TRANSFER_PERIOD 10'000 // microsec
+#define CURR_UPDATE_PERIOD 8       // ms
 
 RobotState state = RobotState_init_zero;
 RobotEffort effort = RobotEffort_init_zero;
@@ -23,71 +23,74 @@ bit0: cannot recv
 uint8_t flags = 0;
 
 void recv() {
-    /* Create a stream that reads from the buffer. */
-    pb_istream_t stream = pb_istream_from_buffer(buffer, sizeof(buffer));
+  /* Create a stream that reads from the buffer. */
+  pb_istream_t stream = pb_istream_from_buffer(buffer, sizeof(buffer));
 
-    /* Now we are ready to decode the message. */
-    pb_decode(&stream, RobotEffort_fields, &effort);
+  /* Now we are ready to decode the message. */
+  pb_decode(&stream, RobotEffort_fields, &effort);
 
-    actuation::cb(effort.lead_screw, effort.lin_act);
-    drivetrain::cb(effort.left_drive, effort.right_drive);
-    deposition::cb(effort.deposit);
-    excavation::cb(effort.excavate);
+  actuation::cb(effort.lead_screw, effort.lin_act);
+  drivetrain::cb(effort.left_drive, effort.right_drive);
+  deposition::cb(effort.deposit);
+  excavation::cb(effort.excavate);
 }
 
 void send() {
-    actuation::update(state.act_right_curr, state.lead_screw_curr,
-                      state.act_ang, state.lead_screw_ang);
-    drivetrain::update(state.drive_left_curr, state.drive_right_curr,
-                       state.drive_left_ang, state.drive_right_ang);
-    deposition::update(state.dep_curr, state.dep_ang);
+  actuation::update(state.act_right_curr, state.lead_screw_curr, state.act_ang,
+                    state.lead_screw_ang);
+  drivetrain::update(state.drive_left_curr, state.drive_right_curr,
+                     state.drive_left_ang, state.drive_right_ang);
+  deposition::update(state.dep_curr, state.dep_ang);
 
-    excavation::update(state.exc_curr);
+  excavation::update(state.exc_curr);
 
-    pb_ostream_t stream = pb_ostream_from_buffer(buffer, sizeof(buffer));
-    pb_encode(&stream, RobotState_fields, &state);
+  uwb::update(state.uwb_dist_0, state.uwb_dist_1, state.uwb_dist_2);
+
+  pb_ostream_t stream = pb_ostream_from_buffer(buffer, sizeof(buffer));
+  pb_encode(&stream, RobotState_fields, &state);
 }
 IntervalTimer enc_timer;
+IntervalTimer uwb_timer;
 
 void setup() {
-    STMotorInterface::init_serial(ST_SERIAL, ST_BAUD_RATE);
-    CurrentSensorBus::init_ads1115();
-    EncoderBus::init();
+  STMotorInterface::init_serial(ST_SERIAL, ST_BAUD_RATE);
+  CurrentSensorBus::init_ads1115();
+  EncoderBus::init();
 
-    enc_timer.begin(EncoderBus::transfer, ENC_TRANSFER_PERIOD);
+  enc_timer.begin(EncoderBus::transfer, ENC_TRANSFER_PERIOD);
+  uwb_timer.begin(UWBBus::transfer, UWB_TRANSFER_PERIOD);
 
-    // disable timeout
-    MC1.setTimeout(0);
-    MC2.setTimeout(0);
-    MC3.setTimeout(0);
-    MC4.setTimeout(0);
+  // disable timeout
+  MC1.setTimeout(0);
+  MC2.setTimeout(0);
+  MC3.setTimeout(0);
+  MC4.setTimeout(0);
 
-    // set to fast ramp (1-10 - fast, 11-20 slow, 20-80 intermed)
-    MC1.setRamping(1);
-    MC2.setRamping(1);
-    MC3.setRamping(1);
-    MC4.setRamping(1);
+  // set to fast ramp (1-10 - fast, 11-20 slow, 20-80 intermed)
+  MC1.setRamping(1);
+  MC2.setRamping(1);
+  MC3.setRamping(1);
+  MC4.setRamping(1);
 }
 
 elapsedMillis ms_until_send;
 elapsedMillis ms_curr_update;
 
 void loop() {
-    int n;
-    n = RawHID.recv(buffer, 0); // 0 timeout = do not wait
-    if (n > 0) {
-        recv();
-    }
+  int n;
+  n = RawHID.recv(buffer, 0); // 0 timeout = do not wait
+  if (n > 0) {
+    recv();
+  }
 
-    if (ms_until_send > TX_PERIOD) {
-        ms_until_send -= TX_PERIOD;
-        send();
-        n = RawHID.send(buffer, 0);
-    }
+  if (ms_until_send > TX_PERIOD) {
+    ms_until_send -= TX_PERIOD;
+    send();
+    n = RawHID.send(buffer, 0);
+  }
 
-    if (ms_curr_update > CURR_UPDATE_PERIOD) {
-        ms_curr_update -= CURR_UPDATE_PERIOD;	
-	CurrentSensorBus::transfer();
-    }
-  
+  if (ms_curr_update > CURR_UPDATE_PERIOD) {
+    ms_curr_update -= CURR_UPDATE_PERIOD;
+    CurrentSensorBus::transfer();
+  }
 }
