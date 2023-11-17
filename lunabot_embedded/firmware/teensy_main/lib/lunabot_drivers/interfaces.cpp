8aa0a695909f909bc5e703cdd434e00f5aeb833c
Raghava Uppuluri <raghava.upp13@gmail.com>
2023-11-17 15:27:56 -0500

    Feature/uwb firmware (#48)
    * uwb firmware updates
    
    - added lunabot_embedded readme
    
    * Added UWB code to teensy
        - Added new class in interfaces
                - Followed pattern of encoder class
                - Sourced from uwb_sensor_test.ino
        - Used class in robot.cpp in new namespace
                - Followed pattern of other namespaces
                - No read command; unecessary
        - Added new interrupt timer in teensy_main
                - To run code from interfaces
    
    * added compilation and flashing docs, in progress
    
    * added Serial8 to UWB interface
    
    * squashed feature/uwb_firmware
    
    * uwb works on robot, made range 5m
    
    ---------
    
    Co-authored-by: WoozyDragon <WoozyDragon@gmail.com>

--- lunabot_embedded/firmware/teensy_main/lib/lunabot_drivers/interfaces.cpp
+++ lunabot_embedded/firmware/teensy_main/lib/lunabot_drivers/interfaces.cpp
@@ -35,13 +35,13 @@ void StepperInterface::off() {
 
 MotorInterface::MotorInterface(uint8_t PWM_P, uint8_t DIR_P)
     : PWM_P_{PWM_P}, DIR_P_{DIR_P} {
-    pinMode(DIR_P_, OUTPUT);
-    pinMode(PWM_P_, OUTPUT);
+  pinMode(DIR_P_, OUTPUT);
+  pinMode(PWM_P_, OUTPUT);
 }
 
 void MotorInterface::write(uint8_t pwm, MotorDir dir) {
-    digitalWrite(DIR_P_, dir);
-    analogWrite(PWM_P_, pwm);
+  digitalWrite(DIR_P_, dir);
+  analogWrite(PWM_P_, pwm);
 }
 
 // Sabertooth MC Interfacing
@@ -52,14 +52,14 @@ STMotorInterface::STMotorInterface(Sabertooth *s, STMotor m)
     : st_{s}, motor_{m} {}
 
 void STMotorInterface::init_serial(HardwareSerial s, int baud_rate) {
-    s.begin(baud_rate);
-    initialized_serial_ = 1;
+  s.begin(baud_rate);
+  initialized_serial_ = 1;
 }
 void STMotorInterface::write(int8_t power) {
-    if (initialized_serial_) {
-        power = min(power, 127);
-        st_->motor(static_cast<byte>(motor_), power);
-    }
+  if (initialized_serial_) {
+    power = min(power, 127);
+    st_->motor(static_cast<byte>(motor_), power);
+  }
 }
 
 // ---- Sensors ----
@@ -80,21 +80,19 @@ uint8_t CurrentSensorBus::adc0_ch_ = 0;
 uint8_t CurrentSensorBus::adc1_ch_ = 0;
 
 void CurrentSensorBus::init_ads1115() {
-    adc0_.setGain(ADS1115_REG_CONFIG_PGA_4_096V); //  +/-4.096V range = Gain 2
-    adc0_.setSampleRate(
-        ADS1115_REG_CONFIG_DR_128SPS); // 128 SPS, or every 7.8ms
+  adc0_.setGain(ADS1115_REG_CONFIG_PGA_4_096V); //  +/-4.096V range = Gain 2
+  adc0_.setSampleRate(ADS1115_REG_CONFIG_DR_128SPS); // 128 SPS, or every 7.8ms
 
-    adc1_.setGain(ADS1115_REG_CONFIG_PGA_4_096V); //  +/-4.096V range = Gain 2
-    adc1_.setSampleRate(
-        ADS1115_REG_CONFIG_DR_128SPS); // 128 SPS, or every 7.8ms
+  adc1_.setGain(ADS1115_REG_CONFIG_PGA_4_096V); //  +/-4.096V range = Gain 2
+  adc1_.setSampleRate(ADS1115_REG_CONFIG_DR_128SPS); // 128 SPS, or every 7.8ms
 
-    if (!adc0_.testConnection() || !adc1_.testConnection()) {
-        return;
-    }
+  if (!adc0_.testConnection() || !adc1_.testConnection()) {
+    return;
+  }
 
-    adc0_.triggerConversion();
-    adc1_.triggerConversion();
-    initialized_ = 1;
+  adc0_.triggerConversion();
+  adc1_.triggerConversion();
+  initialized_ = 1;
 }
 
 void CurrentSensorBus::transfer() {
@@ -119,6 +117,7 @@ void CurrentSensorBus::transfer() {
             adc1_.triggerConversion(); // Start a conversion.  This immediatly
         }
     }
+  }
 }
 
 int16_t CurrentSensorBus::read(uint8_t bus, uint8_t mux) {
@@ -136,71 +135,139 @@ volatile uint8_t EncoderBus::spi_buffer_[EncoderBus::BUFFER_SIZE] = {0};
 volatile uint32_t EncoderBus::enc_buffer_[EncoderBus::BUS_SIZE] = {0};
 
 void EncoderBus::init() {
-    SPI.begin();
-    pinMode(clk_p_, OUTPUT);
-    pinMode(sel0_p_, OUTPUT);
-    pinMode(sel1_p_, OUTPUT);
-    pinMode(sel2_p_, OUTPUT);
-    select_enc_(curr_id_);
-    digitalWriteFast(clk_p_,
-                     HIGH); // Set CLK line HIGH (to meet the requirements
-                            // of SSI interface)
+  SPI.begin();
+  pinMode(clk_p_, OUTPUT);
+  pinMode(sel0_p_, OUTPUT);
+  pinMode(sel1_p_, OUTPUT);
+  pinMode(sel2_p_, OUTPUT);
+  select_enc_(curr_id_);
+  digitalWriteFast(clk_p_,
+                   HIGH); // Set CLK line HIGH (to meet the requirements
+                          // of SSI interface)
 }
 
 void EncoderBus::select_enc_(uint8_t id) {
-    uint8_t sel0 = id & 1;
-    uint8_t sel1 = id >> 1 & 1;
-    uint8_t sel2 = id >> 2 & 1;
-    digitalWrite(sel0_p_, sel0);
-    digitalWrite(sel1_p_, sel1);
-    digitalWrite(sel2_p_, sel2);
+  uint8_t sel0 = id & 1;
+  uint8_t sel1 = id >> 1 & 1;
+  uint8_t sel2 = id >> 2 & 1;
+  digitalWrite(sel0_p_, sel0);
+  digitalWrite(sel1_p_, sel1);
+  digitalWrite(sel2_p_, sel2);
 }
 
 void EncoderBus::transfer() {
-    digitalWriteFast(clk_p_,
-                     LOW); // Set CLK line LOW (to inform encoder -> latch data)
+  digitalWriteFast(clk_p_,
+                   LOW); // Set CLK line LOW (to inform encoder -> latch data)
 
-    // Before in setup() the pinMode() change the CLK pin function to
-    // output, now we have to enable usage of this pin by SPI port with
-    // calling SPI.begin():
-    SPI.begin();
-    SPI.beginTransaction(spi_settings); // We use transactional API
+  // Before in setup() the pinMode() change the CLK pin function to
+  // output, now we have to enable usage of this pin by SPI port with
+  // calling SPI.begin():
+  SPI.begin();
+  SPI.beginTransaction(spi_settings); // We use transactional API
 
-    for (int i = 0; i < BUFFER_SIZE; i++) {
-        spi_buffer_[i] =
-            SPI.transfer(0xAA); // Transfer anything and read data back
-    }
+  for (int i = 0; i < BUFFER_SIZE; i++) {
+    spi_buffer_[i] = SPI.transfer(0xAA); // Transfer anything and read data back
+  }
 
-    SPI.endTransaction(); // We use transactional API
+  SPI.endTransaction(); // We use transactional API
 
-    pinMode(clk_p_,
-            OUTPUT); // A while before we set CLK pin to be used by SPI
-                     // port, now we have to change it manually...
-    digitalWrite(clk_p_, HIGH); // ... back to idle HIGH
+  pinMode(clk_p_,
+          OUTPUT);            // A while before we set CLK pin to be used by SPI
+                              // port, now we have to change it manually...
+  digitalWrite(clk_p_, HIGH); // ... back to idle HIGH
 
-    delayMicroseconds(
-        27); // Running above 500kHz perform Delay First Clock function
+  delayMicroseconds(
+      27); // Running above 500kHz perform Delay First Clock function
 
-    enc_buffer_[curr_id_] =
-        static_cast<uint32_t>(spi_buffer_[0] << 8) |
-        static_cast<uint32_t>(spi_buffer_[1]); // here transfer8 was made
+  enc_buffer_[curr_id_] =
+      static_cast<uint32_t>(spi_buffer_[0] << 8) |
+      static_cast<uint32_t>(spi_buffer_[1]); // here transfer8 was made
 
-    curr_id_ = (curr_id_ + 1) % BUS_SIZE;
-    select_enc_(curr_id_);
+  curr_id_ = (curr_id_ + 1) % BUS_SIZE;
+  select_enc_(curr_id_);
 }
 
 float EncoderBus::read_enc(uint8_t id) {
-    uint32_t data;
-    noInterrupts();
-    data = enc_buffer_[id];
-    interrupts();
+  uint32_t data;
+  noInterrupts();
+  data = enc_buffer_[id];
+  interrupts();
 
-    // Shift one bit right - (MSB of position was placed on at MSB of
-    // uint32_t, so make it right and shift to make MSB position at 30'th
-    // bit): data = data >> 1;
+  // Shift one bit right - (MSB of position was placed on at MSB of
+  // uint32_t, so make it right and shift to make MSB position at 30'th
+  // bit): data = data >> 1;
 
-    // encoderPosition is placed in front (starting from MSB of uint32_t) so
-    // shift it for 11 bits to align position data right
+  // encoderPosition is placed in front (starting from MSB of uint32_t) so
+  // shift it for 11 bits to align position data right
 
-    return static_cast<float>(data) / static_cast<float>(1 << 16) * 360.0F;
+  return static_cast<float>(data) / static_cast<float>(1 << 16) * 360.0F;
+}
+
+volatile float UWBBus::uwb_buffer_[BUS_SIZE] = {0};
+
+float UWBBus::read_uwb(uint8_t id) {
+  noInterrupts();
+  float data = uwb_buffer_[id];
+  interrupts();
+  return data;
+}
+
+void UWBBus::init() {
+  UWBSerial.begin(115200);
+
+  // serial 2 setup as tag on bot (does dist calc)
+  for (int b = 0; b < 2; b++) { // Repeat twice to stabilize the connection
+    delay(50);
+    UWBSerial.write("AT+anchor_tag=0\r\n"); // Set up the Tag
+    delay(50);
+    UWBSerial.write("AT+interval=5\r\n"); // Set the calculation precision,
+                                         // the larger the response is, the
+                                         // slower it will be
+    delay(50); // 设置计算精度，越大响应越慢
+    UWBSerial.write("AT+switchdis=1\r\n"); // Began to distance 开始测距
+    delay(50);
+    if (b == 0) {
+      UWBSerial.write("AT+RST\r\n"); // RESET 复位
+    }
+  }
+  if (UWBSerial.available()) {
+    delay(3);
+  }
+}
+
+void UWBBus::transfer() {
+  if (UWBSerial.available()) {
+    // Read the original input
+    String originalInput = UWBSerial.readStringUntil('\n');
+    originalInput.trim();
+
+    // Check if the message starts with "anX:" where X is a digit
+    if (originalInput.startsWith("an") && isdigit(originalInput.charAt(2)) &&
+        originalInput.charAt(3) == ':') {
+      // Extract sensor number
+      int sensorNumber = originalInput.charAt(2) - '0';
+
+      // Find the index of the colon
+      int colonIndex = originalInput.indexOf(':');
+
+      // Extract the value part and trim spaces
+      String valueString = originalInput.substring(colonIndex + 1);
+      valueString.trim();
+
+      // Convert the trimmed string to a float
+      float num = valueString.toFloat();
+
+      // Assign the values to d0, d1, or d2 based on sensor number
+      UWBBus::uwb_buffer_[sensorNumber] = num;
+
+      /*
+      if (sensorNumber == 0) {
+              d0 = num;
+      } else if (sensorNumber == 1) {
+              d1 = num;
+      } else if (sensorNumber == 2) {
+              d2 = num;
+      }*/
+    }
+  }
 }
--- lunabot_embedded/firmware/teensy_main/lib/lunabot_drivers/robot.cpp
+++ lunabot_embedded/firmware/teensy_main/lib/lunabot_drivers/robot.cpp
@@ -10,7 +10,6 @@ Sabertooth MC3(130, ST_SERIAL); // bottom
 Sabertooth MC4(131, ST_SERIAL); // high-current
 
 // ADCs
-
 namespace actuation {
 
 STMotorInterface act_right{&MC2, STMotor::M1};
@@ -31,18 +30,18 @@ constexpr uint8_t ACT_ENC_MUX = 2;
 
 void update(int32_t &act_right_curr, int32_t &lead_screw_curr, float &act_angle,
             float &lead_screw_angle) {
-    act_right_curr =
-        CurrentSensorBus::read(ACT_RIGHT_CURR_ADC, ACT_RIGHT_CURR_MUX);
-    lead_screw_curr =
-        CurrentSensorBus::read(LEAD_SCREW_CURR_ADC, LEAD_SCREW_CURR_MUX);
-    lead_screw_angle = EncoderBus::read_enc(LEAD_SCREW_ENC_MUX);
-    act_angle = EncoderBus::read_enc(ACT_ENC_MUX);
+  act_right_curr =
+      CurrentSensorBus::read(ACT_RIGHT_CURR_ADC, ACT_RIGHT_CURR_MUX);
+  lead_screw_curr =
+      CurrentSensorBus::read(LEAD_SCREW_CURR_ADC, LEAD_SCREW_CURR_MUX);
+  lead_screw_angle = EncoderBus::read_enc(LEAD_SCREW_ENC_MUX);
+  act_angle = EncoderBus::read_enc(ACT_ENC_MUX);
 }
 
 void cb(int8_t lead_screw, int8_t lin_act) {
-    act_left.write(-lin_act);
-    act_right.write(lin_act);
-    lead_screw_motor.write(lead_screw);
+  act_left.write(-lin_act);
+  act_right.write(lin_act);
+  lead_screw_motor.write(lead_screw);
 }
 
 } // namespace actuation
@@ -62,21 +61,29 @@ constexpr uint8_t DRIVE_LEFT_MUX = 3;
 
 void update(int32_t &left_curr, int32_t &right_curr, float &left_angle,
             float &right_angle) {
-    left_curr = CurrentSensorBus::read(LEFT_CURR_ADC, LEFT_CURR_MUX);
+  left_curr = CurrentSensorBus::read(LEFT_CURR_ADC, LEFT_CURR_MUX);
 
-    right_curr = CurrentSensorBus::read(RIGHT_CURR_ADC, RIGHT_CURR_MUX);
+  right_curr = CurrentSensorBus::read(RIGHT_CURR_ADC, RIGHT_CURR_MUX);
 
-    left_angle = EncoderBus::read_enc(DRIVE_LEFT_MUX);
-    right_angle = EncoderBus::read_enc(DRIVE_RIGHT_MUX);
+  left_angle = EncoderBus::read_enc(DRIVE_LEFT_MUX);
+  right_angle = EncoderBus::read_enc(DRIVE_RIGHT_MUX);
 }
 
 void cb(int8_t left, int8_t right) {
-    // Tank drive steering
-    left_drive.write(-left);
-    right_drive.write(-right);
+  // Tank drive steering
+  left_drive.write(-left);
+  right_drive.write(-right);
 }
 
 } // namespace drivetrain
+
+namespace uwb {
+void update(float &d0, float &d1, float &d2) {
+  d0 = UWBBus::read_uwb(0);
+  d1 = UWBBus::read_uwb(1);
+  d2 = UWBBus::read_uwb(2);
+}
+} // namespace uwb
 
 namespace excavation {
 STMotorInterface exc_motor{&MC4, STMotor::M1};
@@ -85,7 +92,7 @@ constexpr uint8_t EXC_CURR_ADC = 1;
 constexpr uint8_t EXC_CURR_MUX = 0; // U5 curr_sense_board
 
 void update(int32_t &exc_curr) {
-    exc_curr = CurrentSensorBus::read(EXC_CURR_ADC, EXC_CURR_MUX);
+  exc_curr = CurrentSensorBus::read(EXC_CURR_ADC, EXC_CURR_MUX);
 }
 
 void cb(int8_t speed) { exc_motor.write(speed); }
@@ -99,8 +106,8 @@ constexpr uint8_t DEP_CURR_MUX = 1; // U2 curr_sense_board
 constexpr uint8_t DEP_ENC_MUX = 1;
 
 void update(int32_t &dep_curr, float &dep_angle) {
-    dep_curr = CurrentSensorBus::read(DEP_CURR_ADC, DEP_CURR_MUX);
-    dep_angle = EncoderBus::read_enc(DEP_ENC_MUX);
+  dep_curr = CurrentSensorBus::read(DEP_CURR_ADC, DEP_CURR_MUX);
+  dep_angle = EncoderBus::read_enc(DEP_ENC_MUX);
 }
 
 void cb(int8_t speed) { dep_motor.write(speed); }
