f8760a57f95c870fbd61b5a8546c4712c523d856
Raghava Uppuluri <raghava.upp13@gmail.com>
2024-01-13 09:55:47 -0500

    added docs for teensy compilation + upload

--- lunabot_embedded/firmware/setup_flash_tools.sh
+++ lunabot_embedded/firmware/setup_flash_tools.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
 ## Installs compilation tools
-# get arduino-cli executable (replace with Linux_x86 if necessary)
-wget https://downloads.arduino.cc/arduino-cli/arduino-cli_latest_Linux_ARM64.tar.gz
-tar -xvf arduino-cli_latest_Linux_ARM64.tar.gz
-./arduino-cli config init
+arduino-cli config init
 python3 update_board_manager.py 
-./arduino-cli core install teensy:avr
-./arduino-cli lib install HX711-master
-./arduino-cli lib install ADS1115-Lite
+arduino-cli core install teensy:avr
+arduino-cli lib install HX711-master
+arduino-cli lib install ADS1115-Lite
 
 ## Installs teensy flasher
 sudo apt-get install libusb-dev
@@ -17,4 +14,4 @@ cd teensy_loader_cli
 make
 sudo make install
 cd ..
-
+rm -rf teensy_loader_cli
