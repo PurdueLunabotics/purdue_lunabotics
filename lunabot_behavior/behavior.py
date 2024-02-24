9f0e4cddc99905df44b6755c89d550bf491751d5
Isaac-F24 <isaacfuksman@gmail.com>
2024-02-24 10:56:18 -0500

    Refactor behavior to class

--- lunabot_behavior/behavior.py
+++ lunabot_behavior/behavior.py
@@ -7,83 +7,77 @@ from geometry_msgs.msg import Twist
 from apriltag_ros.msg import AprilTagDetectionArray
 from lunabot_msgs.msg import RobotEffort, RobotSensors
 
-class State:
-    drive_left_curr = 0
-    drive_right_curr = 0
-    drive_left_vel = 0
-    drive_right_vel = 0
+class Behavior:
 
-class Effort:
-    cmd_left = 0
-    cmd_right = 0
+    def robot_state_callback(self, msg: RobotSensors):
+        self.robot_state = msg
 
-def robot_state_cb(self, msg):
-    global robot_state
-    robot_state.drive_left_curr = msg.drive_left_curr
-    robot_state.drive_right_curr = msg.drive_right_curr
-    robot_state.dep_curr = msg.dep_curr
+    def effort_callback(self, msg: RobotEffort):
+        self.robot_effort = msg
 
-def eff_cb(self, msg):
-	global robot_effort
-	robot_effort.cmd_left = msg.drive_left
-	robot_effort.cmd_right = msg.drive_right
+    def apritag_callback(self, msg: AprilTagDetectionArray):
+        if len(msg.detections) != 0:
+            self.found_apriltag = True
+        else:
+            self.found_apriltag = False
 
-def apritag_cb(self, msg):
-    global foundTag
-    if len(msg.detections) != 0:
-        foundTag = True
-    else:
-        foundTag = False
+    def __init__(self):
+        self.robot_state: RobotSensors = RobotSensors()
+        self.robot_effort: RobotEffort = RobotEffort()
 
-foundTag = False
-robot_state = State()
-robot_effort = Effort()
+        self.found_apriltag = False
 
-def main():
-    rospy.init_node('behavior_node')
+        self.effort_publisher = rospy.Publisher("/effort", RobotEffort, queue_size=1, latch=True)
+        self.velocity_publisher = rospy.Publisher("/cmd_vel", Twist, queue_size=1, latch=True)
 
-    effort_pub = rospy.Publisher("/effort", RobotEffort, queue_size=1)
-    vel_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=1)
-    
-    rospy.Subscriber("/d455_front/camera/color/tag_detections", AprilTagDetectionArray, apritag_cb)
-    rospy.Subscriber("/sensors", RobotSensors, robot_state_cb)
-    rospy.Subscriber("/effort", RobotEffort, eff_cb)
+        # TODO change to parameters
+        rospy.Subscriber("/d455_front/camera/color/tag_detections", AprilTagDetectionArray, self.apritag_callback)
+        rospy.Subscriber("/sensors", RobotSensors, self.robot_state_callback)
+        rospy.Subscriber("/effort", RobotEffort, self.effort_callback)
 
-    #create interrupt logic here!
-    # rospy.is_shutdown():
-    # stuck
-    # map change
-    # overcurrent
+        rospy.init_node('behavior_node')
 
-    # notes on interrupts: if no functions use rospy.sleep, then they can all 
-    # just check for the kill signal at the end of every loop they go through. 
-    # that can cause them to return false or the error in an enum (need to decide)
-    # and that can be handled by the main process before continuing. 
-    # How to pick up where we were before? Current state enum flag?
-    # also how to cleanly handle the functions returning different values?
-    # and picking back up from somewhere new specificed by the enum flag?
+    def behavior_loop():
 
-    #startup stuff here
-    ascent.main()
-    init_mapping.main()
+        #create interrupt logic here!
+        # rospy.is_shutdown():
+        # stuck
+        # map change
+        # overcurrent
 
-    while(True):
-        #traverse to mining zone
+        # notes on interrupts: if no functions use rospy.sleep, then they can all 
+        # just check for the kill signal at the end of every loop they go through. 
+        # that can cause them to return false or the error in an enum (need to decide)
+        # and that can be handled by the main process before continuing. 
+        # How to pick up where we were before? Current state enum flag?
+        # also how to cleanly handle the functions returning different values?
+        # and picking back up from somewhere new specificed by the enum flag?
 
-        #plunge
-
-        #trench
-
+        #startup stuff here
         ascent.main()
+        init_mapping.main()
 
-        #berm plan
+        while(True):
+            #traverse to mining zone
 
-        #go to berm
+            #plunge
 
-        #deposit
+            #trench
 
-        #post_deposit
+            ascent.main()
 
-        ascent.main() #why?
+            #berm plan
 
-        #plan to mining zone
+            #go to berm
+
+            #deposit
+
+            #post_deposit
+
+            ascent.main() #why?
+
+            #plan to mining zone
+
+if __name__ == "__main__":
+    behavior = Behavior()
+    behavior.behavior_loop()
