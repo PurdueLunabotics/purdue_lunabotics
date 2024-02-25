2626ef667a4ec7abaf886527e48e2f6fe373c405
woozydragon <woozydragon@gmail.com>
2024-02-24 21:42:11 -0500

    added test codes for load cells

---
+++ lunabot_embedded/firmware/load_cell_test/HX_scale.ino
@@ -0,0 +1,63 @@
+//
+//    FILE: HX_kitchen_scale.ino
+//  AUTHOR: Rob Tillaart
+// PURPOSE: HX711 demo
+//     URL: https://github.com/RobTillaart/HX711
+
+
+// to be tested
+
+#include "HX711.h"
+
+HX711 scale;
+
+uint8_t dataPin = 6;
+uint8_t clockPin = 7;
+float w1, w2, previous = 0;
+
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println(__FILE__);
+  Serial.print("LIBRARY VERSION: ");
+  Serial.println(HX711_LIB_VERSION);
+  Serial.println();
+
+  scale.begin(dataPin, clockPin);
+
+  Serial.print("UNITS: ");
+  Serial.println(scale.get_units(10));
+
+  // load cell factor 20 KG
+  // scale.set_scale(127.15);
+  // load cell factor 5 KG
+  scale.set_offset(131992);
+  scale.set_scale(-110.096122);
+
+  Serial.print("UNITS: ");
+  Serial.println(scale.get_units(10));
+}
+
+
+void loop()
+{
+  // read until stable
+  w1 = scale.get_units(10);
+  delay(100);
+  w2 = scale.get_units();
+  while (abs(w1 - w2) > 10)
+  {
+     w1 = w2;
+     w2 = scale.get_units();
+     delay(100);
+  }
+
+  Serial.print("UNITS: ");
+  Serial.println(w1);
+  delay(100);
+}
+
+
+// -- END OF FILE --
+
