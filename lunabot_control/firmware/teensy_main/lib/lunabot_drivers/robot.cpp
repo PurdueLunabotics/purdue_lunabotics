96e0427565efe006de10b68909aa47e64b43fd41
QinKevin-001 <zihaoqin1via@gmail.com>
2024-03-01 20:38:51 -0500

    Commented out Lead Screw items in robot.cpp and RobotEffort.msg files.

--- lunabot_control/firmware/teensy_main/lib/lunabot_drivers/robot.cpp
+++ lunabot_control/firmware/teensy_main/lib/lunabot_drivers/robot.cpp
@@ -4,20 +4,20 @@ Sabertooth MC1(128, ST_SERIAL);
 Sabertooth MC2(129, ST_SERIAL);
 
 namespace actuation {
-static StepperDir lead_screw_dir;
-StepperInterface lead_screw{19, 22, 20, 23, 200, 90, 30};
+//static StepperDir lead_screw_dir;
+//StepperInterface lead_screw{19, 22, 20, 23, 200, 90, 30};
 STMotorInterface lin_acts{&MC2, STMotor::M1};
 
-void run() { lead_screw.step(lead_screw_dir); }
+//void run() { lead_screw.step(lead_screw_dir); }
 
 void cb(int8_t lead_screw_pwm, int8_t lin_act_pwm) {
-    lead_screw_dir = (lead_screw_pwm > 0) ? EXTEND : RETRACT;
+    //lead_screw_dir = (lead_screw_pwm > 0) ? EXTEND : RETRACT;
     lin_acts.write(lin_act_pwm);
-    if (lead_screw_pwm != 0) {
-        lead_screw.on();
-    } else {
-        lead_screw.off();
-    }
+    //if (lead_screw_pwm != 0) {
+    //    lead_screw.on();
+    //} else {
+    //    lead_screw.off();
+    //}
 }
 } // namespace actuation
 
