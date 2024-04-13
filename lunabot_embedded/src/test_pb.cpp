3a1452107319eb162c58374a8493b0aa5a2ed384
Raghava Uppuluri <raghava.upp13@gmail.com>
2024-04-13 15:30:57 -0400

    moved to gtest, will now run with CI

--- lunabot_embedded/src/test_pb.cpp
+++ lunabot_embedded/src/test_pb.cpp
@@ -1,5 +1,6 @@
 #include <assert.h>
 #include <ctime>
+#include <gtest/gtest.h>
 #include <iostream>
 #include <lunabot_embedded/sensor_proc.h>
 #include <stdarg.h>
@@ -17,10 +18,9 @@ extern "C" {
 
 using namespace std;
 
-RobotSensors state = RobotSensors_init_zero;
-RobotEffort effort = RobotEffort_init_zero;
-
-int main(int argc, char **argv) {
+TEST(ProtobufTestSuite, size_check) {
+  RobotSensors state = RobotSensors_init_zero;
+  RobotEffort effort = RobotEffort_init_zero;
   state.lead_screw_curr = 65535;
   state.act_right_curr = 65535;
   state.dep_curr = 65535;
@@ -43,13 +43,18 @@ int main(int argc, char **argv) {
   size_t sensors_size = 0;
   size_t effort_size = 0;
   bool res = pb_get_encoded_size(&sensors_size, RobotSensors_fields, &state);
-  assert(res);
+  ASSERT_TRUE(res);
   res = pb_get_encoded_size(&effort_size, RobotEffort_fields, &effort);
-  assert(res);
+  ASSERT_TRUE(res);
   cout << "sensors nanopb msg size (bytes): " << sensors_size << endl;
   cout << "effort nanopb msg size (bytes): " << effort_size << endl;
   // rawhid can transfer up to 64 bytes each way at 1khz, this file sanity checks this:
   // https://www.pjrc.com/teensy/rawhid.html
-  assert(sensors_size < 64);
-  assert(effort_size < 64);
+  ASSERT_TRUE(sensors_size < 64);
+  ASSERT_TRUE(effort_size < 64);
+}
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
