85859a298d62e6e80a1822bf2b4b7fb526e35112
AlexR <woozydragon@gmail.com>
2024-04-30 22:10:35 -0400

    moved conversion to teensy side

--- lunabot_embedded/src/teensy_driver_node.cpp
+++ lunabot_embedded/src/teensy_driver_node.cpp
@@ -52,15 +52,16 @@ void recv(ros::Publisher &pub) {
   int status;
   /* Create a stream that reads from the buffer. */
   pb_istream_t stream = pb_istream_from_buffer(buf, sizeof(buf));
+  print(buf);
   /* Now we are ready to decode the message. */
   pb_decode(&stream, RobotSensors_fields, &state);
   lunabot_msgs::RobotSensors state_msg;
 
-  state_msg.act_right_curr = adc_to_current_31A(state.act_right_curr);
-  state_msg.drive_right_curr = adc_to_current_31A(state.drive_right_curr);
-  state_msg.drive_left_curr = adc_to_current_31A(state.drive_left_curr);
-  state_msg.dep_curr = adc_to_current_31A(state.dep_curr);
-  state_msg.exc_curr = adc_to_current_31A(state.exc_curr);
+  state_msg.act_right_curr = state.act_right_curr;
+  state_msg.drive_right_curr = state.drive_right_curr;
+  state_msg.drive_left_curr = state.drive_left_curr;
+  state_msg.dep_curr = state.dep_curr;
+  state_msg.exc_curr = state.exc_curr;
   
 
   // TODO: add timestamp to each value using StampedValue, allowing for more accurate derivative
