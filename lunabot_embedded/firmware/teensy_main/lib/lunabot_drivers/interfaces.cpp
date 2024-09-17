1222d1dca3c43d382700fe8a4247d6b7cbc0d3c5
superpenguin612 <74030080+superpenguin612@users.noreply.github.com>
2024-09-17 19:53:11 -0400

    Add calibration and taring

--- lunabot_embedded/firmware/teensy_main/lib/lunabot_drivers/interfaces.cpp
+++ lunabot_embedded/firmware/teensy_main/lib/lunabot_drivers/interfaces.cpp
@@ -237,10 +237,12 @@ HX711 HX711_Bus::encs[NUM_SENSORS] = {
 void HX711_Bus::init() {
   for (int i = 0; i < NUM_SENSORS; i++) {
     encs[i].begin(PIN_LIST[i * 2], PIN_LIST[i * 2 + 1]);
+    encs[i].set_scale(SCALE_CALIBRATION[i]);
+    encs[i].tare();
   }
 }
 
-float HX711_Bus::read_scale(uint8_t id) { return encs[id].read_average(); }
+float HX711_Bus::read_scale(uint8_t id) { return encs[id].get_units(3); }
 
 volatile float M5Stack_UWB_Trncvr::recv_buffer_[NUM_UWB_TAGS] = {0};
 
