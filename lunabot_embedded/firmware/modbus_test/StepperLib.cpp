a6e7b1fd26d5682bbe12dd13cc8ade760fc0a40a
RobertJN64 <robertjnies@gmail.com>
2024-09-17 21:42:59 -0400

    basic functionality

--- lunabot_embedded/firmware/modbus_test/StepperLib.cpp
+++ lunabot_embedded/firmware/modbus_test/StepperLib.cpp
@@ -6,7 +6,26 @@
 
 struct Addrs {
   uint16_t ControlReg = 0x6002;
+
+  uint16_t PathMode = 0x6200;
+  uint16_t Pos_H = 0x6201;
+  uint16_t Pos_L = 0x6202;
+  uint16_t Speed = 0x6203;
+  uint16_t Acceleration = 0x6204;
+  uint16_t Deceleration = 0x6205;
+
 } Addrs;
+
+struct ControlReg {
+  uint8_t TrigPath = 0x10;
+  uint8_t ESTOP = 0x40;
+} ControlReg;
+
+struct PathMode {
+  uint16_t AbsolutePosMode = 0x0001;
+  uint16_t RelativePosMode = 0x0041;
+  uint16_t VelocityMode = 0x0002;
+} PathMode;
 
 // compute CRC across bits 0 to data_range, store in data_range + 1 and data_range + 2
 void modbusCRC(uint8_t *buf, int data_range) {
@@ -26,8 +45,8 @@ void modbusCRC(uint8_t *buf, int data_range) {
   buf[data_range + 1] = (uint8_t)(crc >> 8);
 }
 
-void setup_motors() {
-  RS485.begin(9600);
+void setup_motors(unsigned long baudrate) {
+  RS485.begin(baudrate);
 }
 
 void write_short_frame(uint8_t motor_id, uint16_t param, uint16_t data) {
@@ -37,14 +56,51 @@ void write_short_frame(uint8_t motor_id, uint16_t param, uint16_t data) {
   RS485.beginTransmission();
   RS485.write(buf, 8);
 
-  Serial.println("Writing data: ");
-  for (int j = 0; j < 8; j++) {
-    Serial.print(buf[j]);
-  }
+  // Serial.println("Writing data: ");
+  // for (int j = 0; j < 8; j++) {
+  //   Serial.print(buf[j]);
+  // }
 
   RS485.endTransmission();
 }
 
 void write_estop(uint8_t motor_id) {
-  write_short_frame(motor_id, Addrs.ControlReg, 0x40);
+  write_short_frame(motor_id, Addrs.ControlReg, ControlReg.ESTOP);
+}
+
+void trigger_motion(uint8_t motor_id) {
+  write_short_frame(motor_id, Addrs.ControlReg, ControlReg.TrigPath);
+}
+
+void write_path_position(uint8_t motor_id, uint32_t position) {
+  uint16_t data_high = (uint16_t)(position >> 16);
+  uint16_t data_low = (uint16_t)(position & 0xFFFF);
+  write_short_frame(motor_id, Addrs.Pos_H, data_high);
+  write_short_frame(motor_id, Addrs.Pos_L, data_low);
+}
+
+void move_at_speed(uint8_t motor_id, uint16_t speed, uint16_t acceleration, uint16_t deceleration) {
+  write_short_frame(motor_id, Addrs.PathMode, PathMode.VelocityMode);
+  write_short_frame(motor_id, Addrs.Speed, speed);
+  write_short_frame(motor_id, Addrs.Acceleration, acceleration);
+  write_short_frame(motor_id, Addrs.Deceleration, deceleration);
+  trigger_motion(motor_id);
+}
+
+void move_to_abs_pos(uint8_t motor_id, uint32_t position, uint16_t speed, uint16_t acceleration, uint16_t deceleration) {
+  write_short_frame(motor_id, Addrs.PathMode, PathMode.AbsolutePosMode);
+  write_path_position(motor_id, position);
+  write_short_frame(motor_id, Addrs.Speed, speed);
+  write_short_frame(motor_id, Addrs.Acceleration, acceleration);
+  write_short_frame(motor_id, Addrs.Deceleration, deceleration);
+  trigger_motion(motor_id);
+}
+
+void move_to_rel_pos(uint8_t motor_id, uint32_t position, uint16_t speed, uint16_t acceleration, uint16_t deceleration) {
+  write_short_frame(motor_id, Addrs.PathMode, PathMode.RelativePosMode);
+  write_path_position(motor_id, position);
+  write_short_frame(motor_id, Addrs.Speed, speed);
+  write_short_frame(motor_id, Addrs.Acceleration, acceleration);
+  write_short_frame(motor_id, Addrs.Deceleration, deceleration);
+  trigger_motion(motor_id);
 }
--- lunabot_embedded/firmware/modbus_test/modbus_test.ino
+++ lunabot_embedded/firmware/modbus_test/modbus_test.ino
@@ -6,6 +6,22 @@ void setup(void) {
   Serial.begin(115200);
   Serial.println("Serial connected");
 
+  setup_motors(9600);
+
+  // SAMPLE 1
+  move_to_abs_pos(MyMotor, 200000);
+  write_estop(MyMotor);
+
+  // SAMPLE 2
+  move_to_rel_pos(MyMotor, 10000);
+  write_estop(MyMotor);
+
+  // SAMPLE 3
+  move_at_speed(MyMotor, 600);
+  write_estop(MyMotor);
+
+  // SAMPLE 4 ish
+  move_to_abs_pos(MyMotor, -200000);
   write_estop(MyMotor);
 }
 
