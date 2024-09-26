e6dea1cbc1f36b16d42edcaf21587ab32b1571ee
Sparsh Bansal <sparshbansal17@gmail.com>
2024-09-26 11:27:55 -0400

    Added Slip Controllers and updated simulation to simulate and correct for slip

--- lunabot_control/src/purePursuitController.py
+++ lunabot_control/src/purePursuitController.py
@@ -5,8 +5,9 @@ from operator import sub, mul
 from matplotlib import pyplot as plt
 from matplotlib import animation
 import numpy as np
+from slipController import SlipController
 
-
+# TODO: Add case for endpoint lookahead
 class PurePursuitController:
     def __init__(self, path, MAX_VELOCITY, MAX_ACCEL, MAX_VEL_CHANGE, LOOKAHEAD, robot_pos):
         self.path = path
@@ -23,9 +24,14 @@ class PurePursuitController:
         self.wheels = [0.0, 0.0]
         self.pos = self.robot_pose[0:2]
         self.angle = self.robot_pose[2]
+        self.actual_lin_ang_wel = [0, 0]
 
-    # To 
     def injection(self, spacing):
+        """Adds points in between sparse waypoints
+
+        Args:
+            spacing (int): distance between points
+        """
         new_points = []
         for i in range(len(self.path) - 1):
             vec = tuple(map(sub, self.path[i + 1], self.path[i]))
@@ -40,7 +46,14 @@ class PurePursuitController:
         new_points.append(self.path[len(self.path) - 1])
         self.path = new_points
 
-    def smoothing(self, weight_data, weight_smooth, tolerance):
+    def smoothing(self, weight_data: float, weight_smooth: float, tolerance: float):
+        """Optimization algorithm to smooth waypoints
+
+        Args:
+            weight_data (float): Usually 1 - weight_smooth
+            weight_smooth (float): How smooth curve should be
+            tolerance (float): Usually 0.00001
+        """
         new_points = [list(ele) for ele in self.path]
         change = tolerance
         while change >= tolerance:
@@ -54,6 +67,11 @@ class PurePursuitController:
         self.path = [tuple(l) for l in new_points]
 
     def dist_along_path(self):
+        """Distance along path for each point
+
+        Returns:
+            List[float]: Distance along curve of waypoint at index i
+        """
         dist = 0
         dists = [0]
         for i in range(1, len(self.path)):
@@ -61,7 +79,16 @@ class PurePursuitController:
             dists.append(dist)
         return dists
 
-    def target_velocity(self, k):
+    def target_velocity(self, k: int):
+        """Generates target velocity using MAX_VEL and MAX_ACCEL and kinematics to calculate desired velocity based on distance between points
+
+
+        Args:
+            k (int): Between 1-5 based on show slow around turns
+
+        Returns:
+            List[float]: Target velocity for waypoint at index i
+        """
         curve = self.curvature_p()
         target_vel = [min(self.MAX_VELOCITY, k / (curve[i] + 1e-30)) for i in range(len(curve))]
 
@@ -72,7 +99,20 @@ class PurePursuitController:
 
         return target_vel
 
+    def target_velocity(self):
+        """Generates target velocity using MAX_VEL as constant across all points
+
+        Returns:
+            List[float]: Target velocity for waypoint at index i
+        """
+        return [self.MAX_VELOCITY] * len(self.path)
+
     def curvature_p(self):
+        """Calculate curvature at each waypoint
+
+        Returns:
+            List[float]: Curvature of waypoint at index i
+        """
         curvatures = [0]
         for i in range(1, len(self.path) - 1):
             x1, y1 = self.path[i - 1]
@@ -98,22 +138,14 @@ class PurePursuitController:
         curvatures.append(0)
         return curvatures
 
-    def controller(self, target_vel, target_accel, actual_vel, kv, ka, kp, ki, kd, last_error, integral, last_time,
-                   current_time):
-        error = target_vel - actual_vel
-        dt = current_time - last_time
-        integral += error * dt
-        derivative = (error - last_error) / dt
-
-        last_error = error
-        last_time = current_time
-
-        return kv * target_vel + ka * target_accel + kp * error + ki * integral + kd * derivative, last_error, integral, last_time
-
     def combine_wheel_vels(self, left, right, wheel_base):
         linear = (left + right) / 2
         angular = (right - left) / wheel_base
         return [linear, angular]
+
+    def get_wheel_vels(self, combined_vels, trackwidth):
+        return [combined_vels[0] + (combined_vels[1] * trackwidth / 2),
+                combined_vels[0] - (combined_vels[1] * trackwidth / 2)]
 
     def closest(self):
         mindist = (
@@ -155,33 +187,64 @@ class PurePursuitController:
         self.t = 0
         self.t_i = 0
         return self.path[self.closest()][0:2]
-    
+
     def calc_heading_err(self, lookahead):
         vec_look = (lookahead[0] - self.robot_pose[0], lookahead[1] - self.robot_pose[1])
+        print("Vec look: ", vec_look)
         robot_vec = (math.cos(self.robot_pose[2]), math.sin(self.robot_pose[2]))
+        print("Robot vec: ", robot_vec)
         return self.angle_between_vectors(vec_look, robot_vec)
-    
-    def forward_kinematics(self, wheels, robot_pose, wheelbase, dt):
-        return (robot_pose[0] + (wheels[0] + wheels[1]) / 2 * dt * math.sin(robot_pose[2]),
-               robot_pose[1] + (wheels[0] + wheels[1]) / 2 * dt * math.cos(robot_pose[2]), 
-               robot_pose[2] + math.atan((wheels[0] - wheels[1]) / wheelbase * dt))
-        
-    def calc_slip(self, robot_pose_kf, actual_robot_pose):
-        return self.subtract_vectors(robot_pose_kf, actual_robot_pose)
-    
-    def subtract_vectors(v1, v2):
-        return tuple(map(sub, v1, v2))
-        
-    def angle_between_vectors(v1, v2):
-    # Implementation of angle_between_vectors method
-    # dot = v1[0] * v2[0] + v1[1] * v2[1]
-    # mag = magnitude(v1) * magnitude(v2)
-    # return math.acos(dot / mag)
-        v1_u = unit_vector(v1)
-        v2_u = unit_vector(v2)
-        return 0.0 if math.isnan(np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))) else np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
 
-        
+    # def forward_kinematics(self, wheels, robot_pose, wheelbase, dt):
+    #     return (robot_pose[0] + (wheels[0] + wheels[1]) / 2 * dt * math.sin(robot_pose[2] + math.pi/2),
+    #             robot_pose[1] + (wheels[0] + wheels[1]) / 2 * dt * math.cos(robot_pose[2] + math.pi/2),
+    #             robot_pose[2] + math.atan((wheels[0] - wheels[1]) / wheelbase * dt))
+
+    def forward_kinematics(self, wheels, robot_pose, wheel_base, delta_time):
+        if wheels[0] == wheels[1]:
+            return (robot_pose[0] + wheels[0] * math.cos(robot_pose[2]) * delta_time,
+                    robot_pose[1] + wheels[0] * math.sin(robot_pose[2]) * delta_time, robot_pose[2])
+        elif wheels[0] == -wheels[1]:
+            return (robot_pose[0], robot_pose[1], robot_pose[2] + (wheels[0] - wheels[1]) / wheel_base * delta_time)
+        else:
+            r = (wheel_base / 2) * ((wheels[0] + wheels[1]) / (wheels[1] - wheels[0]))
+            # print("r", r)
+            w = (wheels[1] - wheels[0]) / wheel_base
+            # print("w", w)
+
+            ICC = [robot_pose[0] - r * math.sin(robot_pose[2]), robot_pose[1] + r * math.cos(robot_pose[2])]
+            # print("ICC", ICC)
+
+            matrix_transform = np.matrix([[math.cos(w * delta_time), -1 * math.sin(w * delta_time), 0],
+                                          [math.sin(w * delta_time), math.cos(w * delta_time), 0], [0, 0, 1]])
+            matrix_a = np.matrix([[robot_pose[0] - ICC[0]], [robot_pose[1] - ICC[1]], [robot_pose[2]]])
+            matrix_b = np.matrix([[ICC[0]], [ICC[1]], [w * delta_time]])
+
+            # print("matrix_transform", matrix_transform)
+            # print("matrix_a", matrix_a)
+            # print("matrix_b", matrix_b)
+            new_pose = matrix_transform * matrix_a + matrix_b
+            # print(new_pose)
+
+            return [float(new_pose[0][0].item()), float(new_pose[1][0].item()), float(new_pose[2][0].item())]
+
+    # Takes velocity slippage
+    def calc_slip(self, robot_vel, target_vel):
+        return self.subtract_vectors(target_vel, robot_vel)
+
+    def subtract_vectors(self, v1, v2):
+        return tuple(map(sub, v1, v2))
+
+    def unit_vector(self, vector):
+        """ Returns the unit vector of the vector.  """
+        return vector / np.linalg.norm(vector)
+
+    def angle_between_vectors(self, v1, v2):
+        # Implementation of angle_between_vectors method
+        # dot = v1[0] * v2[0] + v1[1] * v2[1]
+        # mag = magnitude(v1) * magnitude(v2)
+        # return math.acos(dot / mag)
+        return math.atan2(v1[0] * v2[1] - v1[1] * v2[0], v1[0] * v2[0] + v1[1] * v2[1])
 
     def curvature(self, lookahead):
         side = np.sign(math.sin(3.1415 / 2 - self.robot_pose[2]) * (lookahead[0] - self.robot_pose[0]) - math.cos(
@@ -214,21 +277,23 @@ class PurePursuitController:
             self.wheels[i] = last_wheels[i] + min(float(self.MAX_VEL_CHANGE * dt),
                                                   max(-float(self.MAX_VEL_CHANGE * dt), w - last_wheels[i]))
 
-        # TODO: Add controller when done and output velocities
 
     def simulate(self, dt):
         print("Simulating...")
         self.generate_path(6, 0.1, 0.1, 0.00001)
 
-        target_vels = self.target_velocity(5)
+        target_vels = self.target_velocity()
         circle2 = plt.Circle((self.robot_pose[0], self.robot_pose[1]), self.LOOKAHEAD, color='b', fill=False)
         circle1 = plt.Circle((0, 0), 0.1, color='r', fill=True)
-        vector = plt.quiver(self.robot_pose[0], self.robot_pose[1], 1, 1, angles='xy', scale_units='xy', scale=1)
         circles = [circle2, circle1]
+
+        vel_controller = SlipController(k_1=0.03, k_2=0.03, kp=0.03, ki=0.0001, kd=0.004, kv=1, ka=0.0002)
 
         def init():
             ax.add_patch(circle2)
             ax.add_patch(circle1)
+            vector = plt.quiver(self.robot_pose[0], self.robot_pose[1], 1, 1, angles='xy', scale_units='xy', scale=1)
+            circles.append(vector)
             return circles
 
         # print(len(path3))
@@ -236,6 +301,7 @@ class PurePursuitController:
         ax.scatter(*zip(*self.path))
 
         def animate(i):
+            print("Frame: ", i)
             # TODO: Add wheelbase var
             look = self.lookahead()
             circles[1].center = look
@@ -249,17 +315,44 @@ class PurePursuitController:
                 self.wheels[i] = last_wheels[i] + min(float(self.MAX_VEL_CHANGE * dt),
                                                       max(-float(self.MAX_VEL_CHANGE * dt), w - last_wheels[i]))
                 # self.wheels[i] = random.randrange(850, 1000) * self.wheels[i] / 1000
-                self.wheels[i] = max(min(np.random.normal(0.90, 0.05), 1), 0.5) * self.wheels[i]
 
-            self.pos = (self.pos[0] + (self.wheels[0] + self.wheels[1]) / 2 * dt * math.sin(self.angle),
-                        self.pos[1] + (self.wheels[0] + self.wheels[1]) / 2 * dt * math.cos(self.angle))
-            self.angle += math.atan((self.wheels[0] - self.wheels[1]) / 4 * dt)
+            target_lin_ang_vel = self.combine_wheel_vels(self.wheels[0], self.wheels[1], 4)
+            print("target: ", target_lin_ang_vel)
+
+            slip = self.calc_slip(self.actual_lin_ang_wel, target_lin_ang_vel)
+            print("Slip: ", slip)
+            heading_error = self.calc_heading_err(look)
+            print("Heading err: ", heading_error)
+
+            control_loop_vels = vel_controller.update_vel(slip, heading_error, dt, target_vel=vel, target_accel=self.MAX_ACCELERATION)
+            print("Control loop lin/ang: ", control_loop_vels)
+            self.wheels = self.get_wheel_vels(control_loop_vels, 4)
+
+            print("control loop R/L: ", self.wheels)
+
+            for i in range(len(self.wheels)):
+                self.wheels[i] = max(min(max(min(np.random.normal(0.40, 0.1), 1), 0.1) * self.wheels[i], 1), 0)
+
+            print("Control real R/L: ", self.wheels)
+
+            self.actual_lin_ang_wel = self.combine_wheel_vels(self.wheels[0], self.wheels[1], 4)
+
+            new_pose = self.forward_kinematics(self.wheels, self.robot_pose, 4, dt)
+            self.pos = [new_pose[0], new_pose[1]]
+            self.angle = new_pose[2]
             # pose = forward_kinematics(wheels[0], wheels[1], 4, (pos[0], pos[1], angle), dt * i)
             # pos = pose[0:2]
             # angle = pose[2]
 
             circles[0].center = self.pos[0], self.pos[1]
             self.robot_pose = (self.pos[0], self.pos[1], self.angle)
+            print("Robot pose: ", self.robot_pose)
+
+            circles[2].set_UVC(5 * math.cos(self.robot_pose[2]), 5 * math.sin(self.robot_pose[2]))
+            circles[2].set_offsets((self.robot_pose[0], self.robot_pose[1]))
+
+            print("-" * 24)
+
             return circles
 
             # print(str(wheels) + ", " + str(angle))
@@ -268,7 +361,7 @@ class PurePursuitController:
 
         anim = animation.FuncAnimation(fig, animate,
                                        init_func=init,
-                                       frames=360 * 4,
+                                       frames=360*10,
                                        interval=20,
                                        blit=True)
 
@@ -281,8 +374,7 @@ class PurePursuitController:
 
 def main():
     controller = PurePursuitController([(0, 0), (10, 30), (20, 40), (30, 30), (40, 60), (20, 80), (70, 70), (100, 13)],
-                                       12, 5, 15, 8, (0, 0, 0))
+                                       1.5, 1, 2, 8, (0, 0, math.pi / 2))
     controller.simulate(0.1)
-
 
 main()
--- lunabot_control/src/slipController.py
+++ lunabot_control/src/slipController.py
@@ -1,73 +1,97 @@
 # all meters(etc.) unless otherwise mentioned
 from math import cos, pi, sin
 
+
 # https://robotics.jpl.nasa.gov/media/documents/advanced_robotics_2006.pdf
 class SlipController:
-    waypoints = [(0, 0), (10, 30), (20, 40), (30, 30), (40, 60), (20, 80), (50, 80)]
-    MAX_VELOCITY = 13
-    MAX_ACCELERATION = 0.7
-    k_1 = 1
-    k_2 = 1
-    pose = {'x':0, 'y':0,'theta':0,'vx':0,'vy':0,'vtheta':0}
-    wheelSpeed = [0,0]
     
-    def __init__(self):
-        pass
+    def __init__(self, k_1: float = 0.003, k_2: float = 0.003, kv: float = 0, kp: float = 3, ki: float = 0, kd: float = 0,
+                 ka: float = 0):
+        self.k_1 = k_1
+        self.k_2 = k_2
+        self.kv = kv
+        self.ka = ka
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        self.last_error = 0
+        self.integral = 0
 
-    def slip(self, robot_pose, heading_error, ):
-        wheelSpeeds = []
-        
-        
-        return wheelSpeeds
-    
-        
-        
+    # Combined func to output linear and angular velocity for robot
+    def update_vel(self, robot_vel_slip, heading_error, dt, target_accel=0, target_vel=0):
+        return (self.vel_pid_update(robot_vel_slip, dt, target_accel, target_vel),
+                self.vel_ang_slip_update(robot_vel_slip, heading_error, dt))
+
+    # Slip controller from paper for angular velocity, takes in slip vector and heading diff between robot and lookahead point
+    def vel_ang_slip_update(self, robot_vel_slip, heading_error, dt):
+        return (self.k_1 * heading_error + self.k_2 * robot_vel_slip[1]) / dt
+
+    # Generic PIDF controller for linear velocity correction
+    def vel_pid_update(self, robot_vel_slip, dt, target_accel=0, target_vel=0):
+        """_summary_
+
+        Args:
+            target_vel (_type_): _description_
+            actual_vel (_type_): _description_
+            dt (_type_): _description_
+            target_accel (int, optional): _description_. Defaults to 0.
+
+        Returns:
+            _type_: _description_
+        """
+        error = robot_vel_slip[0]
+        self.integral += error * dt
+        derivative = (error - self.last_error) / dt
+
+        self.last_error = error
+
+        return self.kv * target_vel + self.ka * target_accel + self.kp * error + self.ki * self.integral + self.kd * derivative
+
+
 # Use this to find the theoretical pose (ignores slip)
 class WheelOdometry:
-    pose = {'x':0, 'y':0,'theta':0,'vx':0,'vy':0,'vtheta':0}
-    wheelRadius = .2 
-    driveBase = .75 # distance between center of wheels (left/right) meters
-    lastTime = 0 #s
-    
+    pose = {'x': 0, 'y': 0, 'theta': 0, 'vx': 0, 'vy': 0, 'vtheta': 0}
+    wheelRadius = .2
+    driveBase = .75  # distance between center of wheels (left/right) meters
+    lastTime = 0  # s
+
     def updatePose(self, newTime, wheelSpeeds):
         """Updates the pose based on the accumulated encoder ticks
         of the two wheels. See https://chess.eecs.berkeley.edu/eecs149/documentation/differentialDrive.pdf
         for details.
         """
-        leftVelo = wheelSpeeds[0] #rotations/s
+        leftVelo = wheelSpeeds[0]  # rotations/s
         rightVelo = wheelSpeeds[1]
-        
-        deltaTime = newTime - self.lastTime #s
-        
-        
-        
-        leftTravel = leftVelo*deltaTime*2*pi*self.wheelRadius # rotations/s * s * m = m
-        rightTravel = rightVelo*deltaTime*2*pi*self.wheelRadius
+
+        deltaTime = newTime - self.lastTime  # s
+
+        leftTravel = leftVelo * deltaTime * 2 * pi * self.wheelRadius  # rotations/s * s * m = m
+        rightTravel = rightVelo * deltaTime * 2 * pi * self.wheelRadius
 
         deltaTravel = (rightTravel + leftTravel) / 2
         deltaTheta = (rightTravel - leftTravel) / self.driveBase
 
         if rightTravel == leftTravel:
-            deltaX = leftTravel*cos(self.pose['theta'])
-            deltaY = leftTravel*sin(self.pose['theta'])
+            deltaX = leftTravel * cos(self.pose['theta'])
+            deltaY = leftTravel * sin(self.pose['theta'])
         else:
             radius = deltaTravel / deltaTheta
 
             # Find the instantaneous center of curvature (ICC).
-            iccX = self.pose['x'] - radius*sin(self.pose['theta'])
-            iccY = self.pose['y'] + radius*cos(self.pose['theta'])
+            iccX = self.pose['x'] - radius * sin(self.pose['theta'])
+            iccY = self.pose['y'] + radius * cos(self.pose['theta'])
 
-            deltaX = cos(deltaTheta)*(self.pose['x'] - iccX) \
-                - sin(deltaTheta)*(self.pose['y'] - iccY) \
-                + iccX - self.pose['x']
+            deltaX = cos(deltaTheta) * (self.pose['x'] - iccX) \
+                     - sin(deltaTheta) * (self.pose['y'] - iccY) \
+                     + iccX - self.pose['x']
 
-            deltaY = sin(deltaTheta)*(self.pose['x'] - iccX) \
-                + cos(deltaTheta)*(self.pose['y'] - iccY) \
-                + iccY - self.pose['y']
+            deltaY = sin(deltaTheta) * (self.pose['x'] - iccX) \
+                     + cos(deltaTheta) * (self.pose['y'] - iccY) \
+                     + iccY - self.pose['y']
 
         self.pose["x"] += deltaX
         self.pose["y"] += deltaY
-        self.pose["theta"] = (self.pose['theta'] + deltaTheta) % (2*pi)
+        self.pose["theta"] = (self.pose['theta'] + deltaTheta) % (2 * pi)
         self.pose["vx"] = deltaTravel / deltaTime if deltaTime > 0 else 0.
         self.pose["vy"] = 0
         self.pose["vtheta"] = deltaTheta / deltaTime if deltaTime > 0 else 0.
@@ -79,7 +103,7 @@ class WheelOdometry:
 
     def setPose(self, newPose):
         self.pose = newPose
-        
+
 
 def main():
     pass
