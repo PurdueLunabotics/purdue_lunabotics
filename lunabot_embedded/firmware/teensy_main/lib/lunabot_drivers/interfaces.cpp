21b79326f9e300749d7b76c9b1df8a9f8a9cf4a4
superpenguin612 <74030080+superpenguin612@users.noreply.github.com>
2024-10-15 20:55:16 -0400

    Use both load cells

--- lunabot_embedded/firmware/teensy_main/lib/lunabot_drivers/interfaces.cpp
+++ lunabot_embedded/firmware/teensy_main/lib/lunabot_drivers/interfaces.cpp
@@ -237,7 +237,7 @@ HX711 HX711_Bus::encs[NUM_SENSORS] = {
 void HX711_Bus::init() {
   for (int i = 0; i < NUM_SENSORS; i++) {
     encs[i].begin(PIN_LIST[i * 2], PIN_LIST[i * 2 + 1]);
-    // encs[i].set_scale(SCALE_CALIBRATION[i]);
+    encs[i].set_scale(SCALE_CALIBRATION[i]);
     // encs[i].tare();
   }
 }
