8faf9dae4200124494f5b4505984f3b256a5e0f5
superpenguin612 <74030080+superpenguin612@users.noreply.github.com>
2024-11-05 20:48:31 -0500

    Fix scale

--- lunabot_embedded/firmware/teensy_main/lib/lunabot_drivers/interfaces.cpp
+++ lunabot_embedded/firmware/teensy_main/lib/lunabot_drivers/interfaces.cpp
@@ -237,7 +237,11 @@ HX711 HX711_Bus::encs[NUM_SENSORS] = {
 void HX711_Bus::init() {
   for (int i = 0; i < NUM_SENSORS; i++) {
     encs[i].begin(PIN_LIST[i * 2], PIN_LIST[i * 2 + 1]);
+    if (encs[i].is_ready()) {
+      encs[i].tare();
+    }
     encs[i].set_gain(HX711_CHANNEL_A_GAIN_128);
+    encs[i].set_scale(SCALE_CALIBRATION[i]);
   }
 }
 
@@ -245,8 +249,9 @@ float HX711_Bus::read_scale(uint8_t id) {
   if (encs[id].is_ready()) {
     // since we could start with weight on the load cell, manually subtract zero point instead of
     // taring
-    float val = (encs[id].read() - ZERO_POINT[id]) / SCALE_CALIBRATION[id];
-    if (val == 0) // if load cell is not returning any data, but HX711 is connected
+    float val = encs[id].get_units();
+    // if load cell is not returning any data, but HX711 is connected
+    if (val == 0)
       return -1;
     return val;
   } else {
