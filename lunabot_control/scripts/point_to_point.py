6d5b31ae5948e6fdc10be880f19e33ddf1be6641
pchitiveli <pchitive@purdue.edu>
2025-01-21 21:32:29 -0500

    point to point works in sim

--- lunabot_control/scripts/point_to_point.py
+++ lunabot_control/scripts/point_to_point.py
@@ -8,38 +8,44 @@ from pid_controller import PIDController
 from visualization_msgs.msg import Marker
 from tf.transformations import euler_from_quaternion
 import numpy as np
-from pid_controller import PIDController
-import tf
-from scipy.spatial import distance
 
 
 class PointToPoint:
     def __init__(self):
-        rospy.init_node('point_to_point_node')
+        rospy.init_node("point_to_point_node")
 
-        self.LINEAR_P = 2.
+        self.LINEAR_P = 5.0
         self.LINEAR_I = 0
         self.LINEAR_D = 0
         self.LINEAR_TOLERANCE = 0.1  # meters
-        self.MAX_LINEAR_SPEED = 1.  # m/s
+        self.MAX_LINEAR_SPEED = 0.5  # m/s
         self.linear_pid = PIDController(
-            self.LINEAR_P, self.LINEAR_I, self.LINEAR_D, max_output=self.MAX_LINEAR_SPEED)
+            self.LINEAR_P,
+            self.LINEAR_I,
+            self.LINEAR_D,
+            max_output=self.MAX_LINEAR_SPEED,
+        )
 
-        self.ANGULAR_P = 2.5
+        self.ANGULAR_P = 5.0
         self.ANGULAR_I = 0
         self.ANGULAR_D = 0
         self.ANGULAR_TOLERANCE_DEG = 5
         self.ANGULAR_TOLERANCE_RAD = np.deg2rad(self.ANGULAR_TOLERANCE_DEG)
-        self.MAX_ANGULAR_SPEED_DEG_PER_SEC = 360
+        self.MAX_ANGULAR_SPEED_DEG_PER_SEC = 90
         self.MAX_ANGULAR_SPEED_RAD_PER_SEC = np.deg2rad(
-            self.MAX_ANGULAR_SPEED_DEG_PER_SEC)
+            self.MAX_ANGULAR_SPEED_DEG_PER_SEC
+        )
         self.angular_pid = PIDController(
-            self.ANGULAR_P, self.ANGULAR_I, self.ANGULAR_D, max_output=self.MAX_ANGULAR_SPEED_RAD_PER_SEC)
+            self.ANGULAR_P,
+            self.ANGULAR_I,
+            self.ANGULAR_D,
+            max_output=self.MAX_ANGULAR_SPEED_RAD_PER_SEC,
+        )
 
         self.robot_pose = [None, None, None]  # x, y, heading (rad)
         self.last_pose = [None, None, None]  # for velocity calculations
 
-        self.FREQUENCY = 60.
+        self.FREQUENCY = 60.0
         self.pid_dt = 1 / self.FREQUENCY
         self.prev_pid_time = 0
         self.odom_dt = 1 / self.FREQUENCY
@@ -48,38 +54,49 @@ class PointToPoint:
         self.angular_vel = 0
         self.linear_vel = 0
 
-        self.target_point = [None, None]
         self.odom_velocity = [None, None]
 
         self.at_angle_target = True
         self.at_linear_target = True
 
-        self.prev_linear_error = float('inf')
+        self.prev_linear_error = float("inf")
+
+        self.target_point_index = 0
+        self.target_point = [None, None]
+        self.path = []
 
         # PUBLISHERS ==================================================================================================
-        cmd_vel_topic = rospy.get_param('/cmd_vel_topic', '/cmd_vel')
-        self.cmd_vel_publisher = rospy.Publisher(
-            cmd_vel_topic, Twist, queue_size=10)
+        cmd_vel_topic = rospy.get_param("/cmd_vel_topic", "/cmd_vel")
+        self.cmd_vel_publisher = rospy.Publisher(cmd_vel_topic, Twist, queue_size=10)
 
         # TODO: debugging
         self.angular_disparity_publisher = rospy.Publisher(
-            '/ptp/angular_disparity', Float32, queue_size=10)
+            "/ptp/angular_disparity", Float32, queue_size=10
+        )
         self.linear_disparity_publisher = rospy.Publisher(
-            '/ptp/linear_disparity', Float32, queue_size=10)
-        self.heading_publisher = rospy.Publisher(
-            '/ptp/heading', Float32, queue_size=10)
+            "/ptp/linear_disparity", Float32, queue_size=10
+        )
+        self.heading_publisher = rospy.Publisher("/ptp/heading", Float32, queue_size=10)
         self.angle_target_publisher = rospy.Publisher(
-            '/ptp/angle_target', Float32, queue_size=10)
+            "/ptp/angle_target", Float32, queue_size=10
+        )
         self.pid_linear_publisher = rospy.Publisher(
-            '/ptp/pid_linear', Float32, queue_size=10)
+            "/ptp/pid_linear", Float32, queue_size=10
+        )
         self.pid_angular_publisher = rospy.Publisher(
-            '/ptp/pid_angular', Float32, queue_size=10)
+            "/ptp/pid_angular", Float32, queue_size=10
+        )
         self.path_segment_publisher = rospy.Publisher(
-            '/ptp/current_target', Marker, queue_size=10)
+            "/ptp/current_target", Marker, queue_size=10
+        )
+        self.path_publisher = rospy.Publisher("/ptp/line_path", Marker, queue_size=10)
 
         # SUBSCRIBERS ==================================================================================================
-        odom_topic = rospy.get_param('/odom_topic', '/odom')
+        odom_topic = rospy.get_param("/odom_topic", "/odom")
         rospy.Subscriber(odom_topic, Odometry, self.__odom_callback)
+
+        path_topic = rospy.get_param("/nav/global_path_topic", "/nav/global_path")
+        rospy.Subscriber(path_topic, Path, self.__path_callback)
 
     # ==================================================================================================================
     # CALLBACKS
@@ -87,50 +104,116 @@ class PointToPoint:
 
     def __odom_callback(self, msg: Odometry):
         # self.robot_velocity = [msg.twist.twist.linear, msg.twist.twist.angular]
-        angles = euler_from_quaternion([msg.pose.pose.orientation.x, msg.pose.pose.orientation.y,
-                                       msg.pose.pose.orientation.z, msg.pose.pose.orientation.w])
-        self.robot_pose = (msg.pose.pose.position.x,
-                           msg.pose.pose.position.y, angles[2])
+        angles = euler_from_quaternion(
+            [
+                msg.pose.pose.orientation.x,
+                msg.pose.pose.orientation.y,
+                msg.pose.pose.orientation.z,
+                msg.pose.pose.orientation.w,
+            ]
+        )
+        self.robot_pose = (
+            msg.pose.pose.position.x,
+            msg.pose.pose.position.y,
+            angles[2],
+        )
 
         self.odom_dt = rospy.Time.now().to_sec() - self.prev_odom_time
         self.prev_odom_time = rospy.Time.now().to_sec()
 
-        if (self.robot_pose != [None, None, None] and self.last_pose != [None, None, None] and self.odom_dt != 0):
-            if (self.odom_dt == 0 or self.odom_dt == None):
+        if (
+            self.robot_pose != [None, None, None]
+            and self.last_pose != [None, None, None]
+            and self.odom_dt != 0
+        ):
+            if self.odom_dt == 0 or self.odom_dt is None:
                 self.odom_dt = 1 / self.FREQUENCY  # ensure no div by 0 errors
 
             # linear velocity
-            self.odom_velocity[0] = np.linalg.norm(
-                np.array(self.robot_pose[:2]) - np.array(self.last_pose[:2])) / self.odom_dt
+            self.odom_velocity[0] = (
+                np.linalg.norm(
+                    np.array(self.robot_pose[:2]) - np.array(self.last_pose[:2])
+                )
+                / self.odom_dt
+            )
             # angular velocity
             self.odom_velocity[1] = (
-                self.robot_pose[2] - self.last_pose[2]) / self.odom_dt
+                self.robot_pose[2] - self.last_pose[2]
+            ) / self.odom_dt
 
         # update last position
         self.last_pose = self.robot_pose
 
     def __path_callback(self, msg: Path):
-        # initialize path with robot's initial position
-        current_position = self.robot_pose[:2]
-        # * operator unpacks lists into comma separated values - useful for passing list data into functions
-        complex_path = tuple(*current_position)
-        for point in msg.poses:
+        complex_path = []  # create list for storing all d* points
+        for pose in msg.poses:
             # add all points to complex path
-            complex_path.append(
-                tuple(point.pose.position.x, point.pose.position.y))
+            complex_path.append((pose.pose.position.x, pose.pose.position.y))
 
-        if complex_path != self.path:  # only update path if it's different
-            self.path = complex_path
+        marker_points = []
+        self.path, marker_points = self.__simplify_path(complex_path)
 
-    def __simplify_path(self, points, angle_tolerance):
-        for i in range(len(points) - 3):
-            p1 = points[i]
-            p2 = points[i + 1]
-            p3 = points[i + 2]
+        # initialize target point
+        self.target_point_index = 0
+        self.target_point = self.path[self.target_point_index]
 
-            # find angle deviation of middle point (p2) from p1 and p3
-            # if more than angle_tolerance, keep the point
-        pass
+        # visualize sequence of lines
+        self.__visualize_line_path(marker_points)
+
+    # ==================================================================================================================
+    # PATH PROCESSING
+    # ==================================================================================================================
+
+    def __simplify_path(self, points, difference_threshold=0.97):
+        filtered_points = []
+        marker_points = []
+
+        if len(points) >= 3:
+            for i in range(len(points) - 2):
+                p1 = points[i]
+                p2 = points[i + 1]
+                p3 = points[i + 2]
+
+                # find projection of vectors on each other
+                v1 = np.array(p1) - np.array(p2)
+                v1 /= np.linalg.norm(v1)  # normalize
+                v2 = np.array(p3) - np.array(p2)
+                v2 /= np.linalg.norm(v2)  # normalize
+
+                # calculate projection size
+                projection_size = np.abs(np.dot(v1, v2))
+
+                # if within difference threshold, keep the point
+                if projection_size <= difference_threshold:
+                    if len(filtered_points) != 0:
+                        marker_points.append(
+                            Point(filtered_points[-1][0], filtered_points[-1][1], 0)
+                        )
+                        marker_points.append(Point(p2[0], p2[1], 0))
+
+                    filtered_points.append(p2)
+
+                # add last point to end up in same location
+                if i == len(points) - 3:
+                    filtered_points.append(p3)
+                    marker_points.append(Point(p2[0], p2[1], 0))
+                    marker_points.append(Point(p3[0], p3[1], 0))
+
+        if len(filtered_points) == 0:
+            filtered_points = points
+            marker_points = self.__point_list_to_ros_point_list(points)
+
+        marker_points.insert(0, marker_points[0])
+        marker_points.insert(0, Point(self.robot_pose[0], self.robot_pose[1], 0))
+
+        return filtered_points, marker_points
+
+    def __point_list_to_ros_point_list(self, points):
+        results = []
+        for point in points:
+            results.append(Point(point[0], point[1], 0))
+
+        return results
 
     # ==================================================================================================================
     # MOTION
@@ -148,7 +231,8 @@ class PointToPoint:
         current_pose = pose
         # calculate angle to target from x axis
         pose_target_angle = np.arctan2(
-            point[1] - self.robot_pose[1], point[0] - self.robot_pose[0])
+            point[1] - self.robot_pose[1], point[0] - self.robot_pose[0]
+        )
         # subtract heading to find angle error
         angle_error = pose_target_angle - current_pose[2]
         # check if around-the-world distance is smaller than current different
@@ -160,10 +244,13 @@ class PointToPoint:
         self.at_angle_target = np.abs(angle_error) < self.ANGULAR_TOLERANCE_RAD
 
         if not self.at_angle_target and not self.at_linear_target:
-            self.linear_vel = 0  # stop linear translation if angle error becomes too big
+            self.linear_vel = (
+                0  # stop linear translation if angle error becomes too big
+            )
 
             self.angular_vel = -self.angular_pid.calculate(
-                state=angle_error, dt=self.pid_dt, setpoint=0)
+                state=angle_error, dt=self.pid_dt, setpoint=0
+            )
         else:
             self.angular_vel = 0
 
@@ -179,11 +266,13 @@ class PointToPoint:
         current_pose = pose
         current_location = np.array(current_pose[:2])
         # calculate distance to target as error
-        linear_error = np.linalg.norm(
-            np.array(point[:2]) - current_location)
+        linear_error = np.linalg.norm(np.array(point[:2]) - current_location)
 
         # switch sign of the error if magnitude is increasing beyond tolerance - robot needs to go in negative direction
-        if np.abs(linear_error) - np.abs(self.prev_linear_error) >= self.LINEAR_TOLERANCE:
+        if (
+            np.abs(linear_error) - np.abs(self.prev_linear_error)
+            >= self.LINEAR_TOLERANCE
+        ):
             linear_error = linear_error * -1
 
         # update previous linear error after checking that the magnitude is decreasing
@@ -192,9 +281,12 @@ class PointToPoint:
         # check if robot linear position is within tolerance - if so, terminate linear motion
         self.at_linear_target = np.abs(linear_error) < self.LINEAR_TOLERANCE
 
-        if (not self.at_linear_target) and self.angular_vel == 0:  # only translate if stopped turning
+        if (
+            not self.at_linear_target
+        ) and self.angular_vel == 0:  # only translate if stopped turning
             self.linear_vel = -self.linear_pid.calculate(
-                state=linear_error, dt=self.pid_dt, setpoint=0)
+                state=linear_error, dt=self.pid_dt, setpoint=0
+            )
         else:
             self.linear_vel = 0
 
@@ -207,10 +299,10 @@ class PointToPoint:
         """
 
         if point != [None, None] and self.robot_pose != [None, None, None]:
-            self.target_point = point
+            target = point
 
-            self.__turn_to_point(self.target_point, self.robot_pose)
-            self.__translate_to_point(self.target_point, self.robot_pose)
+            self.__turn_to_point(target, self.robot_pose)
+            self.__translate_to_point(target, self.robot_pose)
         else:
             self.linear_vel = 0
             self.angular_vel = 0
@@ -222,7 +314,7 @@ class PointToPoint:
         self.cmd_vel_publisher.publish(vel)
 
         # visualize path to target
-        if (self.target_point != [None, None]):
+        if self.target_point != [None, None]:
             self.__visualize_path_to_target(target=point)
 
     # ==================================================================================================================
@@ -231,7 +323,7 @@ class PointToPoint:
 
     def __visualize_path_to_target(self, target):
         marker = Marker()
-        # Set the frame, e.g., "map" or "base_link"
+        # Set the frame
         marker.header.frame_id = "odom"
         marker.header.stamp = rospy.Time.now()
         marker.ns = "target"
@@ -240,8 +332,8 @@ class PointToPoint:
         marker.action = Marker.ADD
 
         # Set the position of the point
-        marker.pose.position.x = 0.
-        marker.pose.position.y = 0.
+        marker.pose.position.x = 0.0
+        marker.pose.position.y = 0.0
         marker.pose.position.z = 0.0
         marker.pose.orientation.x = 0.0
         marker.pose.orientation.y = 0.0
@@ -263,6 +355,38 @@ class PointToPoint:
 
         self.path_segment_publisher.publish(marker)
 
+    def __visualize_line_path(self, marker_list):
+        marker = Marker()
+
+        # Set the frame
+        marker.header.frame_id = "odom"
+        marker.header.stamp = rospy.Time.now()
+        marker.ns = "target_paths"
+        marker.id = 0
+        marker.type = Marker.LINE_LIST
+        marker.action = Marker.ADD
+
+        # Set the position of the point
+        marker.pose.position.x = 0.0
+        marker.pose.position.y = 0.0
+        marker.pose.position.z = 0.0
+        marker.pose.orientation.x = 0.0
+        marker.pose.orientation.y = 0.0
+        marker.pose.orientation.z = 0.0
+        marker.pose.orientation.w = 1.0
+
+        # set points to the list of marker points
+        marker.points = marker_list
+
+        # Set line properties
+        marker.scale.x = 0.01
+        marker.color.r = 0.0
+        marker.color.g = 0.0
+        marker.color.b = 1.0
+        marker.color.a = 1.0  # Alpha (transparency)
+
+        self.path_publisher.publish(marker)
+
     # ==================================================================================================================
     # NODE RUNNING
     # ==================================================================================================================
@@ -275,11 +399,16 @@ class PointToPoint:
             self.pid_dt = rospy.Time.now().to_sec() - self.prev_odom_time
             self.prev_pid_time = rospy.Time.now().to_sec()  # update previous time
 
-            if (self.pid_dt == 0 or self.pid_dt == None):
+            if self.pid_dt == 0 or self.pid_dt is None:
                 self.pid_dt = 1 / self.FREQUENCY  # ensure no div by 0 errors
 
-            # feed in dummy target (2,2)
-            self.__move_to_point([2., 1.5, np.pi / 2])
+            if self.path != [] and self.at_angle_target and self.at_linear_target:
+                # check that target point index will be within bounds and increment
+                if self.target_point_index != len(self.path) - 1:
+                    self.target_point_index += 1
+                    self.target_point = self.path[self.target_point_index]
+
+            self.__move_to_point(self.target_point)
 
             rate.sleep()
 
