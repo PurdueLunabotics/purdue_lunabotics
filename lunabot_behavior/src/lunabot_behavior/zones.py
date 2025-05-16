d5257f2eccda617be791057725ad8eb797d30bda
Pranav Chitiveli <pchitive@purdue.edu>
2025-05-16 03:28:46 +0000

    i think this fixes it?

--- lunabot_behavior/src/lunabot_behavior/zones.py
+++ lunabot_behavior/src/lunabot_behavior/zones.py
@@ -48,7 +48,7 @@ class Zone:
         # Make a polygon containing all of the corners of the zone and publish it
         zone = Marker()
         zone.header.stamp = rospy.Time.now()
-        zone.header.frame_id = "odom"
+        zone.header.frame_id = "map"
 
         zone.ns = "zones"
         zone.id = id
@@ -102,12 +102,12 @@ class Zone:
 
         publisher.publish(zone)
 
-def calc_point_from_apriltag(x: float, y: float, apriltag_pose_in_odom: PoseStamped, is_sim: bool)-> 'tuple[float,float]':
+def calc_point_from_apriltag(x: float, y: float, apriltag_pose_in_map: PoseStamped, is_sim: bool)-> 'tuple[float,float]':
     """
     Calculates the (x, y) point from the apriltag's location (given in the odom frame)
     """
 
-    roll, pitch, yaw  = euler_from_quaternion([apriltag_pose_in_odom.pose.orientation.x, apriltag_pose_in_odom.pose.orientation.y, apriltag_pose_in_odom.pose.orientation.z, apriltag_pose_in_odom.pose.orientation.w])
+    roll, pitch, yaw  = euler_from_quaternion([apriltag_pose_in_map.pose.orientation.x, apriltag_pose_in_map.pose.orientation.y, apriltag_pose_in_map.pose.orientation.z, apriltag_pose_in_map.pose.orientation.w])
 
     # Add pi/2 in sim to the initial angle because the apriltag orientation in the odom frame is 90 degrees off (it points to the right instead of outwards)
     # In the real robot, subtract (go the other way)
@@ -117,8 +117,8 @@ def calc_point_from_apriltag(x: float, y: float, apriltag_pose_in_odom: PoseStam
     else:
         yaw += math.pi / 2 
 
-    x0 = apriltag_pose_in_odom.pose.position.x
-    y0 = apriltag_pose_in_odom.pose.position.y
+    x0 = apriltag_pose_in_map.pose.position.x
+    y0 = apriltag_pose_in_map.pose.position.y
 
     x1 = x0 + math.cos(yaw) * x - math.sin(yaw) * y
     y1 = y0 + math.sin(yaw) * x + math.cos(yaw) * y
@@ -149,7 +149,7 @@ def calc_offset(x: float, y: float, apriltag_pose_in_odom: PoseStamped, is_sim: 
     return (x1, y1)
 
 
-def find_mining_zone(apriltag_pose_in_odom: PoseStamped, is_sim: bool)->Zone:
+def find_mining_zone(apriltag_pose_in_map: PoseStamped, is_sim: bool)->Zone:
     """ 
     Calculate the mining zone from the apriltag's location.
     Assume the start apriltag is in the starting zone, 0.5 m +y and 0.1 m +x (0.1 m off of left wall, 0.5 m up)
@@ -173,10 +173,10 @@ def find_mining_zone(apriltag_pose_in_odom: PoseStamped, is_sim: bool)->Zone:
     # DIST_Y = -0.5   # Negative distance is the other direction (downwards)
     # LENGTH_Y = 2.57
 
-    top_left = calc_point_from_apriltag(DIST_X, DIST_Y + LENGTH_Y, apriltag_pose_in_odom, is_sim)
-    top_right = calc_point_from_apriltag(DIST_X + LENGTH_X, DIST_Y + LENGTH_Y, apriltag_pose_in_odom, is_sim)
-    bottom_left = calc_point_from_apriltag(DIST_X, DIST_Y, apriltag_pose_in_odom, is_sim)
-    bottom_right = calc_point_from_apriltag(DIST_X + LENGTH_X, DIST_Y, apriltag_pose_in_odom, is_sim)
+    top_left = calc_point_from_apriltag(DIST_X, DIST_Y + LENGTH_Y, apriltag_pose_in_map, is_sim)
+    top_right = calc_point_from_apriltag(DIST_X + LENGTH_X, DIST_Y + LENGTH_Y, apriltag_pose_in_map, is_sim)
+    bottom_left = calc_point_from_apriltag(DIST_X, DIST_Y, apriltag_pose_in_map, is_sim)
+    bottom_right = calc_point_from_apriltag(DIST_X + LENGTH_X, DIST_Y, apriltag_pose_in_map, is_sim)
 
     return Zone(top_left, top_right, bottom_left, bottom_right)
 
