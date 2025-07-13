cbd0f59e44e79e9d5a5f024bbab02659f7fa3637
Pranav Chitiveli <pchitive@purdue.edu>
2025-07-13 23:59:54 +0000

    robot move now wahoo

--- lunabot_description/launch/robot_description.launch.py
+++ lunabot_description/launch/robot_description.launch.py
@@ -1,11 +1,10 @@
 import os
 from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
-from launch_ros.actions import Node
+from launch_ros.actions import Node, SetParameter
 from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 from launch_ros.parameter_descriptions import ParameterValue
-from launch.actions import ExecuteProcess
 import xacro
 
 def generate_launch_description():
@@ -15,18 +14,30 @@ def generate_launch_description():
     # xacro.process_doc(doc)
     # robot_description = doc.toxml()
 
-    robot_description = ParameterValue(
-        Command([
-            FindExecutable(name='xacro'),
-            ' ',
-            PathJoinSubstitution([
-                FindPackageShare('lunabot_description'),
-                'urdf',
-                'dummy_bot.xacro'
-            ])
-        ]),
-        value_type=str
-    )
+    # robot_description = ParameterValue(
+    #     Command([
+    #         FindExecutable(name='xacro'),
+    #         ' ',
+    #         PathJoinSubstitution([
+    #             FindPackageShare('lunabot_description'),
+    #             'urdf',
+    #             'dummy_bot.xacro'
+    #         ])
+    #     ]),
+    #     value_type=str
+    # )
+
+    # urdf_path = os.path.join(get_package_share_directory("lunabot_description"), "urdf", "dummy_bot.urdf")
+    # with open(urdf_path, "r") as urdf:
+    #     robot_description = urdf.read()
+
+    xacro_file = os.path.join(get_package_share_directory("lunabot_description"), "urdf", "bot.xacro.urdf")
+    # xacro_file = os.path.join(get_package_share_directory("gazebo_ros2_control_demos"),
+    #                           'urdf',
+    #                           'test_diff_drive.xacro.urdf')
+    doc = xacro.parse(open(xacro_file))
+    xacro.process_doc(doc)
+    robot_description = doc.toxml()
 
     robot_state_publisher = Node(
         package='robot_state_publisher',
--- mining_arena_gazebo/launch/gazebo.launch.py
+++ mining_arena_gazebo/launch/gazebo.launch.py
@@ -1,7 +1,9 @@
+from posixpath import join
 from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import IncludeLaunchDescription, TimerAction, ExecuteProcess
+from launch.actions import IncludeLaunchDescription, ExecuteProcess, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import PathJoinSubstitution, FindExecutable, Command
 from launch_ros.substitutions import FindPackageShare
@@ -11,6 +13,19 @@ import os
 import xacro
 
 def generate_launch_description():
+    robot_desc_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('lunabot_description'),
+                'launch',
+                'robot_description.launch.py'
+            ])
+        ])
+    )
+
+    # ====================================
+    # GAZEBO STUFF =======================
+
     os.environ['GAZEBO_MODEL_PATH'] = f"$GAZEBO_MODEL_PATH:{get_package_share_directory('mining_arena_gazebo')}/models/"
 
     gazebo_launch = IncludeLaunchDescription(
@@ -27,71 +42,24 @@ def generate_launch_description():
                 'worlds',
 
                 # OUR WORLD DIR
-                'arena_nasa.world'
+                # 'arena_nasa.world'
 
                 # DUPAGE LOW RES WORLD DIR
-                # 'low_resolution',
-                # 'artemis',
-                # 'artemis_arena.world'
+                'low_resolution',
+                'artemis',
+                'artemis_arena.world'
             ])
         }.items()
     )
-
-    robot_desc_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            PathJoinSubstitution([
-                FindPackageShare('lunabot_description'),
-                'launch',
-                'robot_description.launch.py'
-            ])
-        ])
-    )
-
-    # robot_desc_cmd = Command([
-    #     FindExecutable(name='xacro'),
-    #     ' ',
-    #     PathJoinSubstitution([
-    #         FindPackageShare('lunabot_description'),
-    #         'urdf',
-    #         'dummy_bot.xacro'
-    #     ])
-    # ])
-    # robot_description = ParameterValue(
-    #     robot_desc_cmd,
-    #     value_type=str
-    # )
-
-    # robot_desc_config = xacro.process_file(os.path.join(
-    #     get_package_share_directory("lunabot_description"),
-    #     "urdf",
-    #     "dummy_bot.xacro"
-    # ))
-    # robot_description = robot_desc_config.toxml()
-
-    # robot_state_publisher = Node(
-    #     package='robot_state_publisher',
-    #     executable='robot_state_publisher',
-    #     parameters=[{'robot_description': robot_description}],
-    #     output='screen'
-    # )
-
-    # joint_state_publisher = Node(
-    #     package='joint_state_publisher',
-    #     executable='joint_state_publisher',
-    #     name='joint_state_publisher',
-    #     output='screen'
-    # )
-
-    # ====================================
-    # GAZEBO STUFF =======================
 
     robot_spawn_node = Node(
         package='gazebo_ros',
         executable='spawn_entity.py',
         arguments=[
             '-topic', 'robot_description',
-            '-entity', 'robot',
+            '-entity', 'dummy_bot',
             '-x', '2.5', '-y', '1.75', '-z', '1.0',
+            '-Y', '1.570796327', # yaw
             "--ros-args"
         ],
         output='screen'
@@ -100,25 +68,12 @@ def generate_launch_description():
     # ====================================
     # ROS2 CONTROL =======================
 
-    ros2_control_node = Node(
-        package="controller_manager",
-        executable="ros2_control_node",
-        parameters=[
-            # {'robot_description': robot_desc_config},
-            os.path.join(
-                get_package_share_directory("mining_arena_gazebo"),
-                "config",
-                "diff_drive_controller.yaml"
-            )
-        ],
-        output="screen"
-    )
-
     # Joint state broadcaster
     joint_state_broadcaster_spawner = Node(
         package="controller_manager",
         executable="spawner",
         arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+        output="screen"
     )
     
     # Diff drive controller
@@ -126,50 +81,24 @@ def generate_launch_description():
         package="controller_manager",
         executable="spawner",
         arguments=["diff_drive_controller", "--controller-manager", "/controller_manager"],
+        output='screen'
     )
 
-    # diff_drive_controller = Node(
-    #     package='controller_manager',
-    #     executable='spawner',
-    #     arguments=['diff_drive_controller', '--param-file', os.path.join(
-    #         get_package_share_directory("mining_arena_gazebo"),
-    #         "config",
-    #         "diff_drive_controller.yaml"
-    #     )],
-    #     output='screen',
-    # )
-
-    # diff_drive_controller = TimerAction(
-    # period=5.0,  # wait 5 seconds
-    #     actions=[
-    #         Node(
-    #             package='controller_manager',
-    #             executable='spawner',
-    #             arguments=['diff_drive_controller', '--param-file', os.path.join(
-    #                 get_package_share_directory("mining_arena_gazebo"),
-    #                 "config",
-    #                 "diff_drive_controller.yaml"
-    #             )],
-    #             output='screen',
-    #         )
-    #     ]
-    # )
-
-    # spawn_with_delay = TimerAction(
-    #     period=10.0, # wait 10 sec before spawning to ensure that gazebo does in fact exist and the robot doesn't fall into the abyss
-    #     actions=[robot_spawn_node]
-    # )
-
     return LaunchDescription([
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=robot_spawn_node,
+                on_exit=[joint_state_broadcaster_spawner],
+            )
+        ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=joint_state_broadcaster_spawner,
+                on_exit=[diff_drive_controller_spawner],
+            )
+        ),
+
         gazebo_launch,
-        
         robot_desc_launch,
-        # robot_state_publisher,
-        # joint_state_publisher,
-
-        robot_spawn_node,
-
-        # ros2_control_node,
-        joint_state_broadcaster_spawner,
-        diff_drive_controller_spawner
+        robot_spawn_node
     ])
