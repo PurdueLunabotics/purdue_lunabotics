8f68c2623f2137d40d490241fc768caee5268f9a
AADad578 <52874739+AADad578@users.noreply.github.com>
2025-08-02 15:57:35 -0700

    add install script

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd ~/luna_ws && colcon build && cd src/purdue_lunabotics && source ~/luna_ws/install/setup.bash && ros2 launch $1 $2
+cd ~/luna_ws && colcon build && cd src/purdue_lunabotics && source ~/luna_ws/install/setup.bash
---
+++ install.sh
@@ -0,0 +1,151 @@
+#!/bin/bash -eu
+
+# Humble Install from: https://github.com/qboticslabs/ros_install_noetic/blob/humble/ros2_install_humble.sh
+
+#set -x
+
+name_ros_distro=humble 
+user_name=$(whoami)
+echo "#######################################################################################################################"
+echo ""
+echo ">>> {Starting ROS 2 Humble Installation}"
+echo ""
+echo ">>> {Checking your Ubuntu version} "
+echo ""
+#Getting version and release number of Ubuntu
+version=`lsb_release -sc`
+relesenum=`grep DISTRIB_DESCRIPTION /etc/*-release | awk -F 'Ubuntu ' '{print $2}' | awk -F ' LTS' '{print $1}'`
+echo ">>> {Your Ubuntu version is: [Ubuntu $version $relesenum]}"
+#Checking version is focal, if yes proceed othervice quit
+case $version in
+  "jammy" )
+  ;;
+  *)
+    echo ">>> {ERROR: This script will only work on Ubuntu Jammy (22.04).}"
+    exit 0
+esac
+
+echo ""
+echo ">>> {ROS 2 HUmble is fully compatible with Ubuntu Jammy 22.04}"
+echo ""
+echo "#######################################################################################################################"
+echo ">>> {Step 1: Configure your Ubuntu repositories}"
+echo ""
+#Configure your Ubuntu repositories to allow "restricted," "universe," and "multiverse." You can follow the Ubuntu guide for instructions on doing this. 
+#https://help.ubuntu.com/community/Repositories/Ubuntu
+
+
+locale  # check for UTF-8
+
+sudo apt update 
+sudo apt install locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+locale  # verify settings
+
+
+##############################################################
+
+
+sudo apt install -y software-properties-common
+sudo add-apt-repository universe
+
+echo ""
+echo ">>> {Done: Added Ubuntu repositories}"
+echo ""
+echo "#######################################################################################################################"
+echo ">>> {Step 2: Set up your keys}"
+echo ""
+echo ">>> {Installing curl for adding keys}"
+#Installing curl: Curl instead of the apt-key command, which can be helpful if you are behind a proxy server: 
+#TODO:Checking package is not working sometimes, so disabling it
+#Checking curl is installed or not
+#name=curl
+#which $name > /dev/null 2>&1
+
+#if [ $? == 0 ]; then
+#    echo "Curl is already installed!"
+#else
+#    echo "Curl is not installed,Installing Curl"
+
+
+sudo apt update 
+sudo apt install -y curl openssl
+
+
+#fi
+
+echo "#######################################################################################################################"
+echo ""
+#Adding keys
+echo ">>> {Waiting for adding keys, it will take few seconds}"
+echo ""
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+#Checking return value is OK
+#case $ret in
+#  "OK" )
+#  ;;
+#  *)
+#    echo ">>> {ERROR: Unable to add ROS Humble keys}"
+#    exit 0
+#esac
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+
+echo ">>> {Done: Added Keys}"
+echo ""
+echo "#######################################################################################################################"
+echo ">>> {Step 4: Updating Ubuntu package index, this will take few minutes depend on your network connection}"
+echo ""
+sudo apt update
+sudo apt upgrade 
+
+echo ""
+echo "#######################################################################################################################"
+echo ">>> {Step 5: Install ROS Desktop and Dev Tools}"
+echo "#######################################################################################################################"
+echo ""
+echo ">>>  {Starting ROS installation, this will take about 20 min. It will depends on your internet  connection}"
+echo ""
+sudo apt-get install -y ros-${name_ros_distro}-desktop 
+sudo apt install ros-dev-tools
+echo ""
+echo ""
+echo "#######################################################################################################################"
+echo ">>> {Step 6: Setting ROS Environment, This will add ROS environment to .bashrc.}" 
+echo ">>> { After adding this, you can able to access ROS commands in terminal}"
+echo ""
+echo "source /opt/ros/${name_ros_distro}/setup.bash" >> /home/$user_name/.bashrc
+source /home/$user_name/.bashrc
+echo ""
+echo "#######################################################################################################################"
+echo "> {Step 7: Setting Up Environment}"
+echo ""
+sudo apt install python3-colcon-common-extensions
+mkdir -p /home/$user_name/luna_ws/src
+cd ~/luna_ws/src
+sudo apt-get install git
+git clone --branch ros2-develop https://github.com/PurdueLunabotics/purdue_lunabotics.git
+echo ""
+echo "#######################################################################################################################"
+echo "> {Step 8: Installing Dependencies}"
+echo ""
+cd ~/luna_ws
+rosdep install -i --from-path src --rosdistro humble -y
+sudo apt install ros-humble-gz-ros2-control ros-humble-joint-state-publisher ros-humble-ros-gzharmonic
+sudo curl https://packages.osrfoundation.org/gazebo.gpg --output /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
+sudo apt-get update
+sudo apt-get install gz-harmonic
+cd ~/luna_ws/src/purdue_lunabotics
+source build.sh
+echo ""
+echo "#######################################################################################################################"
+echo "> {Step 9: Run Sim}"
+echo ""
+echo "You will now be able to run the simulation."
+echo "Type [ros2 launch lunabot_bringup sim.launch] to start it"
