b72c33638e6303adfef464f40376b8d8bf441b87
AADad578 <52874739+AADad578@users.noreply.github.com>
2025-08-02 18:07:05 -0700

    retry sourcing if it fails once
    idk why this necessary but for some reason it just fails the first time

--- install.sh
+++ install.sh
@@ -143,8 +143,7 @@ sudo curl https://packages.osrfoundation.org/gazebo.gpg --output /usr/share/keyr
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
 sudo apt-get update
 sudo apt-get install gz-harmonic
-cd ~/luna_ws/src/purdue_lunabotics
-source build.sh
+cd ~/luna_ws/src/purdue_lunabotics && source build.sh || cd ~/luna_ws/src/purdue_lunabotics && source build.sh
 echo ""
 echo "#######################################################################################################################"
 echo "> {Step 9: Run Sim}"
