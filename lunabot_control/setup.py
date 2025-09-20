e3c333e8ad836bdc7a514e4af059a9b716e15ff8
Ankur Raghavan <ankurrag@outlook.com>
2025-09-20 14:10:41 -0400

    fix launch files

--- lunabot_control/setup.py
+++ lunabot_control/setup.py
@@ -18,6 +18,7 @@ setup(
     ('share/ament_index/resource_index/packages',
         ['resource/' + package_name]),
     ('share/' + package_name, ['package.xml']),
+    (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
 ],
   entry_points={
     'console_scripts': [
