d0e20eff973a2b8ca44b5327100244fba17a3430
Brayden Zee <github@blueish.dev>
2025-10-18 13:14:16 -0400

    Update drivetrain constants

--- lunabot_control/src/lunabot_control/drivetrain_controller.py
+++ lunabot_control/src/lunabot_control/drivetrain_controller.py
@@ -2,6 +2,7 @@
 
 from rclpy.node import Node
 import rclpy
+import math
 
 from lunabot_msgs.msg import RobotEffort
 from std_msgs.msg import Float64MultiArray
@@ -11,14 +12,22 @@ class DrivetrainController(Node):
     def __init__(self, **kwargs):
         super().__init__('drivetrain_controller_node', **kwargs)
 
+        self._gearbox_ratio = 50.0
+
         self.effort_subscriber = self.create_subscription(RobotEffort, "/effort", self.effort_callback, 1)
         self.velocity_commands_publisher = self.create_publisher(Float64MultiArray, "/velocity_controller/commands", 10)
 
+    def scale_rpm(self, rpm):
+        return rpm / 60.0 * 2.0 * math.pi / self._gearbox_ratio
+
     def effort_callback(self, effort: RobotEffort):
         output = Float64MultiArray()
+        
+        left = self.scale_rpm(float(effort.left_drive))
+        right = self.scale_rpm(float(effort.right_drive))
 
-        output.data = [float(effort.left_drive) / 500.0, float(effort.left_drive) / 500.0,
-                       float(effort.right_drive) / 500.0, float(effort.right_drive) / 500.0]
+        output.data = [left, left,
+                       right, right]
 
         self.velocity_commands_publisher.publish(output)
 
