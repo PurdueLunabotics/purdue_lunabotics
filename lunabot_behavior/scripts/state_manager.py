0c0174481685e5820c114e03f394452d4aa00ce3
Pranav Chitiveli <xlr9.chitiveli@gmail.com>
2026-01-17 17:36:58 -0500

    main robot error states layout + state transitions

--- lunabot_behavior/scripts/state_manager.py
+++ lunabot_behavior/scripts/state_manager.py
@@ -10,6 +10,7 @@ class Events(Enum):
     SUCCESS = auto()
     STALL = auto()
     NO_PATH = auto()
+    FAIL = auto()
 
 
 class MainStates(Enum):
@@ -31,28 +32,77 @@ class MainStates(Enum):
     RAISE_ACT = auto()
     RAISE_ACT_STALL = auto()
     
-    MOVE_TO_LINKUP = auto()
-    MOVE_TO_LINKUP_STALL = auto()
-    MOVE_TO_LINKUP_NO_PATH = auto()
+    TRAVERSE_TO_LINKUP = auto()
+    TRAVERSE_TO_LINKUP_STALL = auto()
+    TRAVERSE_TO_LINKUP_NO_PATH = auto()
+
+    WAIT_FOR_LINKUP = auto()
+
+    TRAVERSE_TO_BERM = auto()
+    TRAVERSE_TO_BERM_STALL = auto()
+    TRAVERSE_TO_BERM_NO_PATH = auto()
+
+    ALIGN_TO_BERM = auto()
+    ALIGN_TO_BERM_STALL = auto()
+    
+    APPROACH_BERM = auto()
+    APPROACH_BERM_STALL = auto()
     
     DEPOSIT = auto()
     DEPOSIT_STALL = auto()
 
     transitions = {
         (IDLE, Events.SUCCESS): INIT_TO_LINKUP,
+        
         (INIT_TO_LINKUP, Events.SUCCESS): ALIGN_TO_TRENCH,
+        (INIT_TO_LINKUP, Events.STALL): INIT_TO_LINKUP_STALL,
+        (INIT_TO_LINKUP, Events.NO_PATH): INIT_TO_LINKUP_NO_PATH,
+        (INIT_TO_LINKUP_NO_PATH, Events.SUCCESS): INIT_TO_LINKUP,
+        (INIT_TO_LINKUP_STALL, Events.SUCCESS): INIT_TO_LINKUP,
+
         (ALIGN_TO_TRENCH, Events.SUCCESS): PLUNGE_ACT,
+        (ALIGN_TO_TRENCH, Events.STALL): ALIGN_TO_TRENCH_STALL,
+        (ALIGN_TO_TRENCH_STALL, Events.SUCCESS): ALIGN_TO_TRENCH,
+        
         (PLUNGE_ACT, Events.SUCCESS): TRENCH,
+        (PLUNGE_ACT, Events.STALL): PLUNGE_ACT_STALL,
+        (PLUNGE_ACT_STALL, Events.SUCCESS): PLUNGE_ACT,
+        
         (TRENCH, Events.SUCCESS): RAISE_ACT,
-        (RAISE_ACT, Events.SUCCESS): MOVE_TO_LINKUP,
-        (MOVE_TO_LINKUP, Events.SUCCESS): DEPOSIT,
+        (TRENCH, Events.STALL): TRENCH_STALL,
+        (TRENCH_STALL, Events.SUCCESS): TRENCH,
+        
+        (RAISE_ACT, Events.SUCCESS): TRAVERSE_TO_LINKUP,
+        (RAISE_ACT, Events.STALL): RAISE_ACT_STALL,
+        (RAISE_ACT_STALL, Events.SUCCESS): RAISE_ACT,
+        
+        (TRAVERSE_TO_LINKUP, Events.SUCCESS): DEPOSIT,
+        (TRAVERSE_TO_LINKUP, Events.STALL): TRAVERSE_TO_LINKUP_STALL,
+        (TRAVERSE_TO_LINKUP, Events.NO_PATH): TRAVERSE_TO_LINKUP_NO_PATH,
+        (TRAVERSE_TO_LINKUP_STALL, Events.SUCCESS): TRAVERSE_TO_LINKUP,
+        (TRAVERSE_TO_LINKUP_NO_PATH, Events.SUCCESS): TRAVERSE_TO_LINKUP,
+
+        (WAIT_FOR_LINKUP, Events.SUCCESS): DEPOSIT,
+        (WAIT_FOR_LINKUP, Events.FAIL): TRAVERSE_TO_BERM,
+
+        # in case minibot is indisposed and big bot has to make full cycles
+        (TRAVERSE_TO_BERM, Events.SUCCESS): ALIGN_TO_BERM,
+        (TRAVERSE_TO_BERM, Events.STALL): ALIGN_TO_BERM,
+        (TRAVERSE_TO_BERM, Events.NO_PATH): TRAVERSE_TO_BERM_NO_PATH,
+        (TRAVERSE_TO_BERM_STALL, Events.SUCCESS): TRAVERSE_TO_BERM,
+        (TRAVERSE_TO_BERM_NO_PATH, Events.SUCCESS): TRAVERSE_TO_BERM,
+
+        (ALIGN_TO_BERM, Events.SUCCESS): APPROACH_BERM,
+        (APPROACH_BERM, Events.SUCCESS): DEPOSIT,
+        
         (DEPOSIT, Events.SUCCESS): ALIGN_TO_TRENCH,
+        (DEPOSIT, Events.STALL): DEPOSIT_STALL,
+        (DEPOSIT_STALL, Events.SUCCESS): DEPOSIT
     }
 
     @staticmethod
     def get_transition(state, event: Events):
         return MainStates.transitions.get((state, event), None)
-
 
 
 class MiniStates(Enum):
@@ -99,21 +149,12 @@ class StateManager(Node):
         self.activate_mini = False
 
     def update_main_state(self, state, event):
-        # return to previous state if stall is handles
-        if state == MainStates.STALL and event == Events.SUCCESS:
-            self.main_state = self.main_return_state
-            return
-
         next_state = MainStates.get_transition(self.main_state, event)
 
         if next_state is not None:
-            # store current state if stalling
-            if next_state == MainStates.STALL:
-                self.main_return_state = self.main_state
-
             self.main_state = next_state
 
-    def update_mini_state(self, state, event):
+    def update_mini_state(self, state, event): # TODO: update
         # return to previous state if stall is handled
         if state == MiniStates.STALL and event == Events.SUCCESS:
             self.mini_state = self.mini_return_state
@@ -140,25 +181,46 @@ class StateManager(Node):
                 case MainStates.INIT_TO_LINKUP:
                     pass
 
+                case MainStates.INIT_TO_LINKUP_NO_PATH:
+                    pass
+
+                case MainStates.INIT_TO_LINKUP_STALL:
+                    pass
+
                 case MainStates.ALIGN_TO_TRENCH:
+                    pass
+
+                case MainStates.ALIGN_TO_TRENCH_STALL:
                     pass
 
                 case MainStates.PLUNGE_ACT:
                     pass
 
+                case MainStates.PLUNGE_ACT_STALL:
+                    pass
+
                 case MainStates.TRENCH:
+                    pass
+
+                case MainStates.TRENCH_STALL:
                     pass
 
                 case MainStates.RAISE_ACT:
                     pass
 
-                case MainStates.MOVE_TO_LINKUP:
+                case MainStates.RAISE_ACT_STALL:
+                    pass
+
+                case MainStates.TRAVERSE_TO_LINKUP:
+                    pass
+
+                case MainStates.TRAVERSE_TO_LINKUP_NO_PATH:
+                    pass
+
+                case MainStates.TRAVERSE_TO_LINKUP_STALL:
                     pass
 
                 case MainStates.DEPOSIT:
-                    pass
-
-                case MainStates.STALL:
                     pass
 
             if event is not None:
