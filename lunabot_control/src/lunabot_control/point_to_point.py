80dc0b4562701ee0cb5fde0b57a7499024120a12
Brayden Zee <github@blueish.dev>
2026-01-17 11:27:00 -0500

    Update point to point to use parameters
    - The parameters can be updated live

--- lunabot_control/src/lunabot_control/point_to_point.py
+++ lunabot_control/src/lunabot_control/point_to_point.py
@@ -1,35 +1,52 @@
 #!/usr/bin/env python3
 
+import threading
+from enum import Enum
+
+import numpy as np
+from rcl_interfaces.msg import SetParametersResult, ParameterDescriptor, ParameterType
 import rclpy
+from geometry_msgs.msg import Point, Pose2D, PoseStamped, Twist
+from nav_msgs.msg import Odometry, Path
 from rclpy.node import Node
 from std_msgs.msg import Bool, Float32, String
-from nav_msgs.msg import Path, Odometry
-from geometry_msgs.msg import Twist, Point, Pose2D, PoseStamped
-from lunabot_control.pid_controller import PIDController
-from visualization_msgs.msg import Marker
 from tf_transformations import euler_from_quaternion
-import numpy as np
-from enum import Enum
-import threading
+from visualization_msgs.msg import Marker
+
+from lunabot_control.pid_controller import PIDController
 
 
 class States(Enum):
     MOVING_TO_ANGULAR_TARGET = 1
     MOVING_TO_LINEAR_TARGET = 2
-    AT_DESTINATION = 4 #at final path point
+    AT_DESTINATION = 4  # at final path point
 
 
 class PointToPoint(Node):
     def __init__(self, **kwargs):
-        super().__init__('point_to_point_node', **kwargs)
+        super().__init__("point_to_point_node", **kwargs)
         rclpy.get_global_executor().add_node(self)
         # self.get_logger().info("init")
 
-        self.LINEAR_P = 5.0
-        self.LINEAR_I = 0
-        self.LINEAR_D = 0
-        self.LINEAR_TOLERANCE = 0.2  # meters
-        self.MAX_LINEAR_SPEED = 0.3  # m/s
+        self.declare_parameters("linear", [("p", 3.0, ParameterDescriptor(type = ParameterType.PARAMETER_DOUBLE)),
+                                           ("i", 0.0, ParameterDescriptor(type = ParameterType.PARAMETER_DOUBLE)),
+                                           ("d", 0.0, ParameterDescriptor(type = ParameterType.PARAMETER_DOUBLE)),
+                                           ("max_speed", 0.3, ParameterDescriptor(type = ParameterType.PARAMETER_DOUBLE)),
+                                           ("tolerance", 0.2, ParameterDescriptor(type = ParameterType.PARAMETER_DOUBLE))])
+
+        self.declare_parameters("angular", [("p", 5.0, ParameterDescriptor(type = ParameterType.PARAMETER_DOUBLE)),
+                                           ("i", 0.0, ParameterDescriptor(type = ParameterType.PARAMETER_DOUBLE)),
+                                           ("d", 0.0, ParameterDescriptor(type = ParameterType.PARAMETER_DOUBLE)),
+                                           ("max_speed", 60.0, ParameterDescriptor(type = ParameterType.PARAMETER_DOUBLE)),
+                                           ("tolerance", 10.0, ParameterDescriptor(type = ParameterType.PARAMETER_DOUBLE))])
+
+        self.declare_parameter("frequency", 60.0, ParameterDescriptor(type = ParameterType.PARAMETER_DOUBLE))
+
+        self.LINEAR_P = self.get_parameter("linear.p").get_parameter_value().double_value
+        self.LINEAR_I = self.get_parameter("linear.i").get_parameter_value().double_value
+        self.LINEAR_D = self.get_parameter("linear.d").get_parameter_value().double_value
+        self.LINEAR_TOLERANCE = self.get_parameter("linear.tolerance").get_parameter_value().double_value  # meters
+        self.MAX_LINEAR_SPEED = self.get_parameter("linear.max_speed").get_parameter_value().double_value  # m/s
         self.linear_pid = PIDController(
             self.LINEAR_P,
             self.LINEAR_I,
@@ -37,12 +54,12 @@ class PointToPoint(Node):
             max_output=self.MAX_LINEAR_SPEED,
         )
 
-        self.ANGULAR_P = 5.0
-        self.ANGULAR_I = 0
-        self.ANGULAR_D = 0
-        self.ANGULAR_TOLERANCE_DEG = 10
+        self.ANGULAR_P = self.get_parameter("angular.p").get_parameter_value().double_value
+        self.ANGULAR_I = self.get_parameter("angular.i").get_parameter_value().double_value
+        self.ANGULAR_D = self.get_parameter("angular.i").get_parameter_value().double_value
+        self.ANGULAR_TOLERANCE_DEG = self.get_parameter("angular.tolerance").get_parameter_value().double_value
         self.ANGULAR_TOLERANCE_RAD = np.deg2rad(self.ANGULAR_TOLERANCE_DEG)
-        self.MAX_ANGULAR_SPEED_DEG_PER_SEC = 60
+        self.MAX_ANGULAR_SPEED_DEG_PER_SEC = self.get_parameter("angular.max_speed").get_parameter_value().double_value
         self.MAX_ANGULAR_SPEED_RAD_PER_SEC = np.deg2rad(
             self.MAX_ANGULAR_SPEED_DEG_PER_SEC
         )
@@ -56,7 +73,7 @@ class PointToPoint(Node):
         self.robot_pose = [None, None, None]  # x, y, heading (rad)
         self.last_pose = [None, None, None]  # for velocity calculations
 
-        self.FREQUENCY = 60.0
+        self.FREQUENCY = self.get_parameter("frequency").get_parameter_value().double_value
         self.pid_dt = 1 / self.FREQUENCY
         self.prev_pid_time = 0
         self.odom_dt = 1 / self.FREQUENCY
@@ -85,7 +102,6 @@ class PointToPoint(Node):
         self.is_enabled = True
 
         self.print_debug_info: bool = False
-        
 
         # PUBLISHERS ==================================================================================================
         cmd_vel_topic = "cmd_vel"
@@ -102,9 +118,7 @@ class PointToPoint(Node):
         self.angle_target_publisher = self.create_publisher(
             Float32, "ptp/angle_target", 10
         )
-        self.pid_linear_publisher = self.create_publisher(
-            Float32, "ptp/pid_linear", 10
-        )
+        self.pid_linear_publisher = self.create_publisher(Float32, "ptp/pid_linear", 10)
         self.pid_angular_publisher = self.create_publisher(
             Float32, "ptp/pid_angular", 10
         )
@@ -115,29 +129,24 @@ class PointToPoint(Node):
 
         self.path_publisher = self.create_publisher(Marker, "ptp/line_path", 10)
 
-        self.state_publisher = self.create_publisher(
-            String, "ptp/robot_state", 10
-        )
+        self.state_publisher = self.create_publisher(String, "ptp/robot_state", 10)
 
-        self.target_publisher = self.create_publisher(
-            Pose2D, "ptp/target_pose", 10
-        )
-        self.log_publisher = self.create_publisher(
-            String, "ptp/log", 10
-        )
-
+        self.target_publisher = self.create_publisher(Pose2D, "ptp/target_pose", 10)
+        self.log_publisher = self.create_publisher(String, "ptp/log", 10)
         # SUBSCRIBERS ==================================================================================================
         odom_topic = "odom"
         self.create_subscription(PoseStamped, odom_topic, self.__odom_callback, 1)
 
         path_topic = "global_path"
         self.create_subscription(Path, path_topic, self.__path_callback, 1)
-        
+
         backwards_topic = "backwards"
         self.create_subscription(Bool, backwards_topic, self.__backwards_callback, 1)
-        
+
         traversal_topic = "traversal_enabled"
         self.create_subscription(Bool, traversal_topic, self.__traversal_callback, 1)
+
+        self.add_on_set_parameters_callback(self.__parameter_callback)
 
     # ==================================================================================================================
     # CALLBACKS
@@ -145,10 +154,10 @@ class PointToPoint(Node):
 
     def __backwards_callback(self, msg: Bool):
         self.is_moving_backwards = msg.data
-        
+
     def __traversal_callback(self, msg: Bool):
         self.is_enabled = msg.data
-        if(not self.is_enabled):
+        if not self.is_enabled:
             self.cmd_vel_publisher.publish(Twist())
 
     def __odom_callback(self, msg: PoseStamped):
@@ -165,10 +174,14 @@ class PointToPoint(Node):
         self.robot_pose = (
             msg.pose.position.x,
             msg.pose.position.y,
-            (angles[2]) %(2*np.pi) - np.pi  if self.is_moving_backwards else angles[2],  # -pi to pi
+            (angles[2]) % (2 * np.pi) - np.pi
+            if self.is_moving_backwards
+            else angles[2],  # -pi to pi
         )
 
-        self.odom_dt = self.get_clock().now().seconds_nanoseconds()[0] - self.prev_odom_time
+        self.odom_dt = (
+            self.get_clock().now().seconds_nanoseconds()[0] - self.prev_odom_time
+        )
         self.prev_odom_time = self.get_clock().now().seconds_nanoseconds()[0]
 
         if (
@@ -194,13 +207,11 @@ class PointToPoint(Node):
         # update last position
         self.last_pose = self.robot_pose
 
-    def __path_callback(self, msg: Path): 
-        
+    def __path_callback(self, msg: Path):
         if len(msg.poses) == 0:
             self.__visualize_line_path([])
             self.target_pose = [None, None, None]
             return
-
 
         self.path = []  # create list for storing all d* poses
         for pose in msg.poses:
@@ -220,6 +231,37 @@ class PointToPoint(Node):
         self.target_pose_index = 0
         self.target_pose = self.path[self.target_pose_index]
 
+    def __parameter_callback(self, params: list[rclpy.Parameter]):
+        for param in params:
+            if param.type_ != rclpy.Parameter.Type.DOUBLE:
+                self.get_logger().error(f"Invalid parameter type for {param.name}")
+                return SetParametersResult(successful = False, reason = f"Invalid parameter type for {param.name}")
+
+            if param.name == "linear.p":
+                self.linear_pid.kp = param.get_parameter_value().double_value
+            elif param.name == "linear.d":
+                self.linear_pid.kd = param.get_parameter_value().double_value
+            elif param.name == "linear.i":
+                self.linear_pid.ki = param.get_parameter_value().double_value
+            elif param.name == "linear.max_speed":
+                self.linear_pid.max_output = param.get_parameter_value().double_value
+            elif param.name == "linear.tolerance":
+                self.LINEAR_TOLERANCE = param.get_parameter_value().double_value
+            elif param.name == "angular.p":
+                self.angular_pid.kp = param.get_parameter_value().double_value
+            elif param.name == "angular.d":
+                self.angular_pid.kd = param.get_parameter_value().double_value
+            elif param.name == "angular.i":
+                self.angular_pid.ki = param.get_parameter_value().double_value
+            elif param.name == "angular.max_speed":
+                self.angular_pid.max_output = np.deg2rad(param.get_parameter_value().double_value)
+            elif param.name == "angular.tolerance":
+                self.ANGULAR_TOLERANCE_RAD = np.deg2rad(param.get_parameter_value().double_value)
+            else:
+                self.get_logger().error(f"Unknown parameter: {param.name}")
+                return SetParametersResult(successful = False, reason = f"Unknown parameter: {param.name}")
+
+        return SetParametersResult(successful = True)
 
     # ==================================================================================================================
     # STATE PROCESSING
@@ -233,8 +275,8 @@ class PointToPoint(Node):
             target (list-like): target 2D pose of format (x, y, theta)
             pose (list-like): current robot 2D pose in format (x, y, theta)
             path (list-like): sequence of target poses in the path, each of which are in format (x, y, theta)
-        """        
-        
+        """
+
         # store x and y coords of pose in a location variable
         current_location = np.array(current_pose[:2])
 
@@ -254,8 +296,11 @@ class PointToPoint(Node):
         # check if robot linear position is within tolerance - if so, terminate linear motion
         self.at_linear_target = np.abs(self.linear_error) < self.LINEAR_TOLERANCE
 
-        #ensure that error is negative if robot overshoots target
-        if (np.abs(self.linear_error) - np.abs(self.prev_linear_error) >= self.LINEAR_TOLERANCE):
+        # ensure that error is negative if robot overshoots target
+        if (
+            np.abs(self.linear_error) - np.abs(self.prev_linear_error)
+            >= self.LINEAR_TOLERANCE
+        ):
             self.linear_error = self.linear_error * -1
 
         # update previous linear error after checking that the magnitude is decreasing
@@ -283,9 +328,8 @@ class PointToPoint(Node):
         if np.abs(2 * np.pi - np.abs(self.angle_error)) < np.abs(self.angle_error):
             # normalize to make error reflect around-the-world
             self.angle_error = 2 * np.pi - np.abs(self.angle_error)
-            #ensure that the direction is correct
+            # ensure that the direction is correct
             self.angle_error *= -1 if pose_target_angle - current_pose[2] > 0 else 1
-            
 
         # check if robot heading is within tolerance - if so, terminate turning procedure
         self.at_angle_target = np.abs(self.angle_error) < self.ANGULAR_TOLERANCE_RAD
@@ -294,14 +338,14 @@ class PointToPoint(Node):
         ## UPDATE STATE -------
         ## -------------------------------------------------
 
-        if (self.print_debug_info):
-            self.get_logger().info(f'''
+        if self.print_debug_info:
+            self.get_logger().info(f"""
                    PTP ------------------------------- \n
                    At Linear Target: {self.at_linear_target} \n
                    At Angular Target: {self.at_angle_target} \n
                    On Final Trajectory: {on_final_trajectory} \n
                    -----------------------------------
-                   ''')
+                   """)
         # self.log_publisher.publish(f'''
         #            PTP ------------------------------- \n
         #            At Linear Target: {self.at_linear_target} \n
@@ -334,7 +378,7 @@ class PointToPoint(Node):
             self.angular_vel = self.angular_pid.calculate(
                 state=self.angle_error, dt=self.pid_dt, setpoint=0
             )
-            
+
         elif self.state == States.MOVING_TO_LINEAR_TARGET:
             self.angular_vel = 0
 
@@ -369,10 +413,19 @@ class PointToPoint(Node):
         marker.pose.orientation.z = 0.0
         marker.pose.orientation.w = 1.0
 
-        if self.robot_pose[0]==None or self.robot_pose[1]==None or self.target_pose[0]==None or self.target_pose[1]==None:
-           return 
-        start_point = Point(x=float(self.robot_pose[0]), y=float(self.robot_pose[1]), z=0.0)
-        end_point = Point(x=float(self.target_pose[0]), y=float(self.target_pose[1]), z=0.0)
+        if (
+            self.robot_pose[0] == None
+            or self.robot_pose[1] == None
+            or self.target_pose[0] == None
+            or self.target_pose[1] == None
+        ):
+            return
+        start_point = Point(
+            x=float(self.robot_pose[0]), y=float(self.robot_pose[1]), z=0.0
+        )
+        end_point = Point(
+            x=float(self.target_pose[0]), y=float(self.target_pose[1]), z=0.0
+        )
 
         marker.points.append(start_point)
         marker.points.append(end_point)
@@ -431,7 +484,7 @@ class PointToPoint(Node):
         vel = Twist()
         vel.linear.x = float(self.linear_vel)
         vel.angular.z = float(self.angular_vel)
-        
+
         vel.linear.x *= -1 if self.is_moving_backwards else 1
         self.cmd_vel_publisher.publish(vel)
 
@@ -454,17 +507,25 @@ class PointToPoint(Node):
 
         while rclpy.ok():
             # self.get_logger().info("loop")
-            if (self.is_enabled):
+            if self.is_enabled:
                 pose = self.robot_pose
 
                 # update difference in time
-                self.pid_dt = self.get_clock().now().seconds_nanoseconds()[0] - self.prev_pid_time
-                self.prev_pid_time = self.get_clock().now().seconds_nanoseconds()[0]  # update previous time
+                self.pid_dt = (
+                    self.get_clock().now().seconds_nanoseconds()[0] - self.prev_pid_time
+                )
+                self.prev_pid_time = (
+                    self.get_clock().now().seconds_nanoseconds()[0]
+                )  # update previous time
 
                 if self.pid_dt == 0 or self.pid_dt is None:  # ensure no div by 0 errors
                     self.pid_dt = 1 / self.FREQUENCY
 
-                if self.target_pose != [None, None, None] and pose != [None, None, None]:
+                if self.target_pose != [None, None, None] and pose != [
+                    None,
+                    None,
+                    None,
+                ]:
                     self.__update_state(pose)
                     self.__move_to_point()
                 else:
