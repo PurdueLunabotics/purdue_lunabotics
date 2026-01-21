9c4c0a5ea0bfe0435bf074eefa6664145d6c9e23
Brayden Zee <github@blueish.dev>
2026-01-20 21:00:16 -0500

    Fix the mini bot urdf and remove nav2 controller test stuff

--- lunabot_control/setup.py
+++ lunabot_control/setup.py
@@ -22,7 +22,6 @@ setup(
   entry_points={
     'console_scripts': [
         'point_to_point_node = lunabot_control.point_to_point:main',
-        'nav2_localclient_node = lunabot_control.nav2_localclient:main',
         'differential_drive_controller_node = lunabot_control.differential_drive_controller:main',
         'effort_factory_node = lunabot_control.effort_factory:main',
         'manual_controller_node = lunabot_control.manual_controller:main',
