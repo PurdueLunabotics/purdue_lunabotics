71ee1b5f3639cad3e73177714d83d566ebdb3c0c
Brayden Zee <github@blueish.dev>
2026-02-07 10:44:36 -0500

    Filter apriltags to only use our static ones for slam

--- lunabot_perception/scripts/apriltag_combine.py
+++ lunabot_perception/scripts/apriltag_combine.py
@@ -9,12 +9,15 @@ class ApriltagCombine(Node):
         super().__init__("apriltag_combine_node", **kwargs)
 
         self.declare_parameter("input_topics", ["example"], ParameterDescriptor(type = ParameterType.PARAMETER_STRING_ARRAY))
+        self.declare_parameter("input_tags", [11, 7], ParameterDescriptor(type = ParameterType.PARAMETER_INTEGER_ARRAY))
 
         self.subs = [self.create_subscription(AprilTagDetectionArray, topic, self.detection_cb, 10)
                      for topic in self.get_parameter("input_topics").get_parameter_value().string_array_value]
         self.pub = self.create_publisher(AprilTagDetectionArray, "detections", 1)
+        self.input_tags = self.get_parameter("input_tags").get_parameter_value().integer_array_value
 
     def detection_cb(self, detections: AprilTagDetectionArray):
+        detections.detections = list(filter(lambda detection: detection.id in self.input_tags, detections.detections))
         self.pub.publish(detections)
 
 def main():
