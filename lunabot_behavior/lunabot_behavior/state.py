25fdef7aa8fd9dbcaf7fa387a8d314227c70949d
Ankur Raghavan <ankurrag@outlook.com>
2026-02-10 21:49:42 -0500

    Revert "revert adding lidar to behavior"
    This reverts commit b561e8254a5f9b0cd94892d60b5833a8fba93317.

--- lunabot_behavior/lunabot_behavior/state.py
+++ lunabot_behavior/lunabot_behavior/state.py
@@ -7,9 +7,10 @@ class Events(Enum):
     FAIL = auto()
 
     # External
-    STALL = auto()
-    NO_PATH = auto()
-    STUCK = auto()
+    STALL = 11
+    NO_PATH = 12
+    STUCK = 13
+    ARRIVED = 14
 
 class State:
     def setup(self, manager: Node):
