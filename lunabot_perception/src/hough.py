3623f5e805290bd590c09c17010737459b906ae5
Alex C <alex2cool4@gmail.com>
2026-02-18 09:09:20 -0500

    adds hough trnasforms and general crater generation

---
+++ lunabot_perception/src/hough.py
@@ -0,0 +1,100 @@
+import numpy as np
+from hough_transform import hough_transform_ring
+
+
+def get_bin_centers(
+    c_x, c_y, r, uncertainty
+):
+    cx_bin_centers = np.linspace(
+        start=c_x - 0.5*uncertainty,
+        stop=c_x + 0.5*uncertainty,
+        num=11)
+    cy_bin_centers = np.linspace(
+        start=c_y - 0.5*uncertainty,
+        stop=c_y + 0.5*uncertainty,
+        num=11)
+    r_bin_centers = np.linspace(
+        start=r - 0.25,
+        stop=r + 0.25,
+        num=11)
+    return cx_bin_centers, cy_bin_centers, r_bin_centers
+
+
+def interpretHoughSpace(
+    houghSpace
+):
+    indices_of_maximum_value = np.unravel_index(
+        np.argmax(houghSpace), shape=houghSpace.shape)
+    return indices_of_maximum_value
+
+
+def advanced_guess_with_hough(
+    guessed_cx, guessed_cy, guessed_r, point_cloud,
+    uncertainty, epsilon
+):
+    cx_bin_centers, cy_bin_centers, r_bin_centers = (
+        get_bin_centers(guessed_cx, guessed_cy, guessed_r, uncertainty)
+    )
+    epsilon = np.float32(epsilon)
+    cx_bin_centers = cx_bin_centers.astype(np.float32)
+    cy_bin_centers = cy_bin_centers.astype(np.float32)
+    r_bin_centers = r_bin_centers.astype(np.float32)
+    point_positions = point_cloud.astype(np.float32)
+    houghSpace = hough_transform_ring(
+        point_positions, cx_bin_centers,
+        cy_bin_centers, r_bin_centers, epsilon
+    )
+    location_for_maxima = interpretHoughSpace(houghSpace)
+    hough_cx_idx = int(location_for_maxima[0])
+    hough_cy_idx = int(location_for_maxima[1])
+    hough_r_idx = int(location_for_maxima[2])
+    hough_cx = cx_bin_centers[hough_cx_idx]
+    hough_cy = cy_bin_centers[hough_cy_idx]
+    hough_r = r_bin_centers[hough_r_idx]
+    return hough_cx, hough_cy, hough_r
+
+
+def compare_old_new(previous_muon_features, muon_features):
+    old_cx = previous_muon_features['cx']
+    old_cy = previous_muon_features['cy']
+    old_r = previous_muon_features['r']
+    cx = muon_features['cx']
+    cy = muon_features['cy']
+    r = muon_features['r']
+    d_cx = abs(old_cx)-abs(cx)
+    d_cy = abs(old_cy)-abs(cy)
+    d_r = abs(old_r)-abs(r)
+    return d_cx, d_cy, d_r
+
+
+def hough_pointcloud(
+    guessed_cx, guessed_cy, guessed_r,
+    point_cloud, uncertainty, epsilon,
+    max_iter=20
+):
+    ring_features = {}
+    ring_features['cx'] = guessed_cx
+    ring_features['cy'] = guessed_cy
+    ring_features['r'] = guessed_r
+    i = 0
+    while i < max_iter:
+        i += 1
+        previous_ring_features = ring_features.copy()
+        hough_cx, hough_cy, hough_r = advanced_guess_with_hough(
+            ring_features['cx'], ring_features['cy'],
+            ring_features['r'], point_cloud, uncertainty, epsilon)
+        ring_features['cx'] = hough_cx
+        ring_features['cy'] = hough_cy
+        ring_features['r'] = hough_r
+        d_cx, d_cy, d_r = compare_old_new(
+            previous_ring_features, ring_features
+        )
+        uncertainty /= 2
+        if (
+            d_cx <= np.deg2rad(0.05) and
+            d_cy <= np.deg2rad(0.05) and
+            d_r <= np.deg2rad(0.03) and
+            i >= 6
+        ):
+            break
+    return hough_cx, hough_cy, hough_r
---
+++ lunabot_perception/src/hough_transform.py
@@ -0,0 +1,85 @@
+import numpy as np
+import math
+
+def hough_transform_ring (point_positions, cx_bin_centers,  cy_bin_centers, r_bin_centers, hough_epsilon: np.float32):
+    i_cx = 0
+    i_cy = 0
+    i_r = 0
+    cx_max = cx_bin_centers.shape[0]
+    cy_max = cy_bin_centers.shape[0]
+    r_max = r_bin_centers.shape[0]
+    contribution : np.float32 
+    houghSpace : np.ndarray[np.float32] = np.zeros(
+        [cx_max, cy_max, r_max],
+        dtype=np.float32
+    )
+    for i_cx in range(cx_max):
+        for i_cy in range(cy_max):
+            for i_r in range(r_max):
+                
+                contribution = sum_point_contributions(
+                    point_positions, 
+                    cx_bin_centers[i_cx],
+                    cy_bin_centers[i_cy], 
+                    r_bin_centers[i_r], 
+                    hough_epsilon
+                )
+                houghSpace[i_cx, i_cy, i_r] = contribution
+    return houghSpace
+
+
+def sum_point_contributions(
+    point_positions,
+    cx : np.float32, 
+    cy : np.float32, 
+    r : np.float32, 
+    hough_epsilon : np.float32
+):
+    total_amplitude : np.float32 = 0
+    point_x : np.float32
+    point_y : np.float32
+    r_point : np.float32
+    point_contribution : np.float32
+    point_positions_max = point_positions.shape[0]
+    for i_point in range(point_positions_max):
+        point_x = point_positions[i_point, 0]
+        point_y = point_positions[i_point, 1]
+        r_point = calculate_point_distance(
+            ring_cx=cx, ring_cy=cy,
+            point_x=point_x, point_y=point_y)
+        point_contribution = apply_triangular_evaluation(
+            ring_radius=r, amplitude=1,
+            r_point=r_point, hough_epsilon=hough_epsilon)
+        total_amplitude += point_contribution
+    return total_amplitude
+
+
+def calculate_point_distance(
+    ring_cx: np.float32,
+    ring_cy: np.float32,
+    point_x: np.float32,
+    point_y : np.float32
+):
+    x_pos : np.float32 = point_x-ring_cx
+    y_pos : np.float32 = point_y-ring_cy
+    r_point : np.float32 = math.sqrt(x_pos**2 + y_pos**2)
+    return r_point
+
+def apply_triangular_evaluation(
+    r_point : np.float32,
+    hough_epsilon : np.float32,
+    ring_radius : np.float32,
+    amplitude : np.float32
+):
+    abs_loc : np.float32
+    point_contribution : np.float32
+    if (
+        r_point > ring_radius-hough_epsilon and
+        r_point < ring_radius+hough_epsilon
+    ):
+        abs_loc = abs(r_point-ring_radius)
+        point_contribution = amplitude*(
+            hough_epsilon-abs_loc)/(hough_epsilon)
+    else:
+        point_contribution = 0
+    return point_contribution
