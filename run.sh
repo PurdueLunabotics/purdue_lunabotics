6db8171fa1a16107432fe403caf06a0a35ea2422
Purdue Lunabotics Team <purduelunaboticsteam@gmail.com>
2026-02-20 16:43:21 -0500

    fix cameras and params

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export GZ_VERSION=harmonic
-cd ~/luna_ws && source ~/.bashrc &&colcon build && cd src/purdue_lunabotics && source ~/luna_ws/install/setup.bash && ros2 launch $@
+cd ~/luna_ws && colcon build && source ~/.bashrc && cd src/purdue_lunabotics && source ~/luna_ws/install/setup.bash && ros2 launch $@
