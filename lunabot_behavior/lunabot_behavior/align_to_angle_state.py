1c8e759551d099b8552b553884401d02ea528600
Brayden Zee <github@blueish.dev>
2026-02-23 15:32:36 -0500

    Fix align to angle state
    - change odom topic to position
    - add tolerance
    - fix angle error

--- lunabot_behavior/lunabot_behavior/align_to_angle_state.py
+++ lunabot_behavior/lunabot_behavior/align_to_angle_state.py
@@ -7,14 +7,15 @@ import numpy as np
 
 class AlignToAngleState(State):
   def __init__(self, angle, **kwargs):
-    self.target_angle = np.deg2rad(angle)
+    self.target_angle = np.deg2rad(angle) % (2 * np.pi)
     
   def setup(self, manager:Node):
     self.cmd_vel_publisher = manager.create_publisher(Twist, "cmd_vel", 10)
-    manager.create_subscription(PoseStamped, "odom", self.odom_cb, 1)
+    manager.create_subscription(PoseStamped, "position", self.odom_cb, 1)
     self.manager = manager
     self.robot_pose = (None, None, None)
     self.angular_speed = np.deg2rad(60) #degrees/sec -> rad/sec
+    self.tolerance = np.deg2rad(3)
   
   def odom_cb(self, msg:PoseStamped):
     angles = euler_from_quaternion(
@@ -28,7 +29,7 @@ class AlignToAngleState(State):
     self.robot_pose = (
         msg.pose.position.x,
         msg.pose.position.y,
-        angles[2],  # -pi to pi
+        angles[2] % (2 * np.pi),  # 0 to 2pi
     )
     
   def start(self):
@@ -37,8 +38,11 @@ class AlignToAngleState(State):
   def periodic(self):
     if self.robot_pose[0] == None:
       return None
-    angular_error = self.target_angle - self.robot_pose[2]
-    angular_error = (angular_error + np.pi) % (2 * np.pi) - np.pi
+    angular_error = self.robot_pose[2] - self.target_angle
+    if angular_error > np.pi:
+      angular_error -= 2 * np.pi
+    elif angular_error < -np.pi:
+      angular_error += 2 * np.pi
     
     if self.robot_pose[2] != None and np.abs(angular_error) < self.tolerance:
       return Events.SUCCESS
