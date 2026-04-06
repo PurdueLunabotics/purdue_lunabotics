57ad81f55369b17c393f84254e8fe65b194d348f
Brayden Zee <github@blueish.dev>
2026-04-06 17:12:30 -0400

    Add new find linkup algorithm

--- lunabot_behavior/setup.py
+++ lunabot_behavior/setup.py
@@ -26,7 +26,7 @@ setup(
     entry_points={
         'console_scripts': [
             "zones = lunabot_behavior.zones:main",
-            "find_linkup = lunabot_behavior.find_linkup:main",
+            "find_linkup = lunabot_behavior.states.find_linkup:main",
             "main = lunabot_behavior.main_states:main",
             "mini = lunabot_behavior.mini_states:main",
             "single = lunabot_behavior.single_states:main",
