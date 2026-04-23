ecf4d5457c86674bf8053da4bb9b46aaff973d0d
Purdue Lunabotics Team <purduelunaboticsteam@gmail.com>
2026-04-23 19:19:33 -0400

    fixed?

--- lunabot_control/src/lunabot_control/manual_controller.py
+++ lunabot_control/src/lunabot_control/manual_controller.py
@@ -224,7 +224,7 @@ class ManualController(Node):
             # Dpad up/down - control linear actuators
             effort_msg.lin_act = int(constrain(joy.axes[Axes.DPAD_VERTICAL.value]) * self.ACTUATE_SPEED)
 
-            effort_msg.dep_servo = int(joy.axes[Axes.DPAD_HORIZONTAL.value]+1 * self.SERVO_POS)/2
+            effort_msg.dep_servo = int(int((joy.axes[Axes.DPAD_HORIZONTAL.value]+1) * self.SERVO_POS)/2)
             # Deposition- B to go, view/select/back to move backwards
             if (joy.buttons[Buttons.B.value] == 1):
                 effort_msg.deposit = int(self.DEPOSITION_SPEED)
