7eb44df862d46b35f8c0ceb4b5943a3369fad53b
Alex C <alex2cool4@gmail.com>
2026-04-27 18:33:48 -0400

    pushing an attempt at cpp but it's never used anywhere. so kinda ignore this push

--- lunabot_perception/src/hough_transform.hpp
+++ lunabot_perception/src/hough_transform.hpp
@@ -1,32 +1,29 @@
-#ifndef HOUGH_TRANSFORM_HPP
-#define HOUGH_TRANSFORM_HPP
+#define hough_transform
 
-std::array<float32> HoughTransform::houghTransformRing (
-    std::array<float32> point_positions,
-    std::array<float32> cx_bin_centers,  
-    std::array<float32> cy_bin_centers, 
-    std::array<float32> r_bin_centers, 
-    float32 hough_epsilon);
+std::vector<std::vector<std::vector<float>>> houghTransformRing (
+    std::vector<std::vector<float>>* point_positions,
+    std::vector<float> cx_bin_centers,  
+    std::vector<float> cy_bin_centers, 
+    std::vector<float> r_bin_centers, 
+    float hough_epsilon);
 
-float32 HoughTransform::sumPointContributions(
-    std::array<float32> point_positions,
-    float32 cx, 
-    float32 cy, 
-    float32 r, 
-    float32 hough_epsilon);
+float sumPointContributions(
+    std::vector<std::vector<float>>* point_positions,
+    float cx, 
+    float cy, 
+    float r, 
+    float hough_epsilon);
 
-float32 HoughTransform::calculate_point_distance(
-    float32 ring_cx,
-    float32 ring_cy,
-    float32 point_x,
-    float32 point_y
+float calculatePointDistance(
+    float ring_cx,
+    float ring_cy,
+    float point_x,
+    float point_y
 );
 
-float32 HoughTransform::apply_triangular_evaluation(
-    float32 r_point,
-    float32 hough_epsilon,
-    float32 ring_radius,
-    float32 amplitude
+float applyTriangularEvaluation(
+    float r_point,
+    float hough_epsilon,
+    float ring_radius,
+    float amplitude
 );
-
-#endif
