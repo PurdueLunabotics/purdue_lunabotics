1ca0858c0d54c2d0c8245a18bd31c1d73fa8a7fc
Purdue Lunabotics Team <purduelunaboticsteam@gmail.com>
2026-04-27 18:46:24 -0400

    whoops

--- lunabot_control/src/lunabot_control/manual_controller.py
+++ lunabot_control/src/lunabot_control/manual_controller.py
@@ -125,13 +125,8 @@ class ManualController(Node):
         self.latched_excavation_speed = 0
         self.excavation_is_latched = False
 
-<<<<<<< Updated upstream
         self.DEPOSITION_SPEED = -1500 if self.robot_num == 1 else 1500
         self.ACTUATE_SPEED = 0.8 # percentage of max power
-=======
-        self.DEPOSITION_SPEED = -1500 
-        self.ACTUATE_SPEED = 0.5 # percentage of max power
->>>>>>> Stashed changes
         self.SERVO_POS = 500 # percentage of max power
         self.EXCAVATION_SPEED = 2000 
 
