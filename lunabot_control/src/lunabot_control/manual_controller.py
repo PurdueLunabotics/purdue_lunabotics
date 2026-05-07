37f8a1712cbbff3d6c1bbcca344e6a94a9e95419
Purdue Lunabotics Team <purduelunaboticsteam@gmail.com>
2026-05-07 17:39:39 -0400

    make manual control deposition go the right way

--- lunabot_control/src/lunabot_control/manual_controller.py
+++ lunabot_control/src/lunabot_control/manual_controller.py
@@ -127,7 +127,7 @@ class ManualController(Node):
         self.latched_excavation_speed = 0
         self.excavation_is_latched = False
 
-        self.DEPOSITION_SPEED = -1500 if self.robot_num == 1 else 1500
+        self.DEPOSITION_SPEED = 1500 
         self.ACTUATE_SPEED = 0.8 # percentage of max power
         self.SERVO_POS = 500 # percentage of max power
         self.EXCAVATION_SPEED = 2000 
