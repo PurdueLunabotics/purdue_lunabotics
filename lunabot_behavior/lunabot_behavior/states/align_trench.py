ec61e27d12bcc028a51f886f4d3a1d9d89d91d88
Purdue Lunabotics Team <purduelunaboticsteam@gmail.com>
2026-05-14 22:34:03 -0400

    Changes to smart excavation

--- lunabot_behavior/lunabot_behavior/states/align_trench.py
+++ lunabot_behavior/lunabot_behavior/states/align_trench.py
@@ -185,8 +185,8 @@ class AlignTrench(AlignToAngle):
 
     cost, blocked = CostmapUtil.line_cost(costmap, a, b, LETHAL_COST)
 
-    if zones.zone_to_poly(zones.exc_zone).contains(a) or zones.zone_to_poly(zones.exc_zone).contains(b):
-      blocked = True
+    # if zones.zone_to_poly(zones.exc_zone).contains(a) or zones.zone_to_poly(zones.exc_zone).contains(b):
+    #   blocked = True
 
     if not blocked:
       self.show_line(a, b, "good", int(np.rad2deg(target_angle)) + 360, r=0.0, g=1.0, b=0.0)
