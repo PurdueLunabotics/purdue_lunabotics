9fafd61c5d0dc36392fa7ebcdbbbe5f13ee1a3bd
Pranav Chitiveli <pchitive@purdue.edu>
2026-05-15 02:04:13 -0400

    fixed error in fullstop and made trenching autonomy states, may want to change trenching to time-based

--- lunabot_behavior/setup.py
+++ lunabot_behavior/setup.py
@@ -38,6 +38,7 @@ setup(
             "ex_align = lunabot_behavior.ex_align_states:main",
             "single = lunabot_behavior.single_states:main",
             "traversal = lunabot_behavior.traversal_states:main",
+            "trench = lunabot_behavior.trench_states:main"
         ],
     },
 )
