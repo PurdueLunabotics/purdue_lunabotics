87a5fd7b0ddcd3c86a03e5a5cd3cc9a9566322a8
Rohan Gopalan <130528515+ElectroRG@users.noreply.github.com>
2026-09-23 00:40:09 +0000

    add CameraInfo publisher to CameraPublisher node

--- lunabot_perception/scripts/camera_publish.py
+++ lunabot_perception/scripts/camera_publish.py
@@ -1,9 +1,11 @@
 import cv2
-
 import rclpy
+
 from rclpy.node import Node
 from cv_bridge import CvBridge
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CameraInfo
+import os
+import yaml
 
 class CameraPublisher(Node):
     def __init__(self):
@@ -11,10 +13,14 @@ class CameraPublisher(Node):
 
         self.declare_parameter("camera_port", 0)
         self.camera_port = self.get_parameter("camera_port").get_parameter_value()
-
         self.get_logger().info(f'Received argument: {self.camera_port}')
-        
-        self.publisher_ = self.create_publisher(Image, 'camera/image', 10)
+
+        self.image_publisher = self.create_publisher(Image, 'camera/image', 10)
+        self.info_publisher = self.create_publisher(CameraInfo, '/camera/camera_info', 10)
+
+        # TODO put in path for camera calibration
+        self.yaml_path = os.path.expanduser('camera_calibration.yaml')
+        self.camera_info_msg = self.load_camera_info(self.yaml_path)
 
         self.bridge = CvBridge()
 
@@ -26,18 +32,52 @@ class CameraPublisher(Node):
             return
 
         # Create a timer which publishes 30 fps
-        self.timer = self.create_timer(1.0 / 30.0, self.publish_frame)
+        self.timer = self.create_timer(1.0 / 30.0, self.timer_callback)
 
         self.get_logger().info("Camera publisher started")
 
-    def publish_frame(self):
+    def load_camera_info(self, yaml_path):
+        msg = CameraInfo()
+
+        # check for the yaml file
+        if not os.path.exists(yaml_path):
+            self.get_logger().error(f"Calibration file not found at {yaml_path}! Low-key gonna publish blank camera info now.")
+
+        with open(yaml_path, 'r') as file:
+            data = yaml.safe_load(file)
+
+        # process yaml calibration data into camera info msg
+        msg.width = data['image.width']
+        msg.height = data['image.height']
+        msg.distortion_model = data['distortion_model']
+
+        msg.d = data['distortion_coefficients']['data']
+        msg.k = data['camera_matrix']['data']
+        msg.r = data['rectification_matrix']['data']
+        msg.p = data['projection_matrix']['data']
+
+        return msg
+
+    def timer_callback(self):
         ret, frame = self.camera.read()
         if not ret:
             self.get_logger().error("Failed to capture image from camera")
             return
 
-        msg = self.bridge.cv2_to_imgmsg(frame, encoding="bgr8")
-        self.publisher_.publish(msg)
+        image_msg = self.bridge.cv2_to_imgmsg(frame, encoding="bgr8")
+        
+        time_stamp = self.get_clock().now().to_msg()
+
+        self.image_msg.header.stamp = time_stamp
+        self.image_msg.header.frame_id = 'camera_optical_frame'   
+
+        self.camera_info_msg.header.stamp = time_stamp
+        self.camera_info_msg.header.frame_id = 'camera_optical_frame'
+        
+        # Publish the messages
+        self.image_publisher.publish(image_msg)
+        self.info_publisher.publish(self.camera_info_msg)
+        self.get_logger().info('Publishing CameraInfo parameters...', once=True)
 
     def destroy_node(self):
         self.camera.release()
